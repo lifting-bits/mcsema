c74db0bf9c7c3a28dda6867cc5acb9b977d7c48b
Artem Dinaburg <artem@trailofbits.com>
2014-08-31 21:53:00 -0700

    Remove commented code

--- mc-sema/binary_common/ELFTarget.h
+++ mc-sema/binary_common/ELFTarget.h
@@ -1,7 +1,8 @@
-#include "bincomm.h"
-#include <LExcn.h>
+#ifndef ELFTARGET_H
+#define ELFTARGET_H
+
+#include <string>
 #include "LLVMObjectTarget.h"
-#include <algorithm>
 
 
 class ElfTarget : public LLVMObjectTarget {
@@ -12,31 +13,6 @@ private:
     ElfTarget();
 protected:
   ElfTarget(const std::string &modname, llvm::object::ObjectFile *of): LLVMObjectTarget(modname, of) {};
-};
+}; 
 
-//class ElfTarget : public ExecutableContainer {
-//    std::string                    mod_name;
-//
-//    llvm::object::ELFObjectFile<llvm::support::little, false>    *elf;
-//    VA                        baseAddr;
-//    VA                        extent;
-//    public:
-//
-//        ElfTarget(std::string f, const llvm::Target *T);
-//
-//        typedef std::pair<boost::uint64_t, llvm::object::SectionRef>  secT;
-//
-//        virtual ~ElfTarget(void);
-//        virtual bool is_open(void);
-//        virtual bool get_exports(std::list<std::pair<std::string, VA> > &);
-//        virtual bool find_import_name(uint32_t, std::string &);
-//        virtual bool is_addr_relocated(uint32_t);
-//        virtual bool relocate_addr(VA, VA &);
-//        virtual bool get_sections(std::vector<ExecutableContainer::SectionDesc>  &);
-//        virtual std::string name(void) { return this->mod_name; }
-//        virtual ::uint64_t getBase(void) const;
-//        virtual ::uint64_t getExtent(void) const;
-//        virtual int readByte(::uint64_t, boost::uint8_t *) const;
-//    private:
-//        std::vector<secT>              secs;
-//};
+#endif
