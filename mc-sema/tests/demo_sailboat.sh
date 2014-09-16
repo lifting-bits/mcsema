bf8246d103c5bc9395875f47efa403bc638619fd
Artem Dinaburg <artem@trailofbits.com>
2014-09-16 14:19:56 -0700

    Sailboat demo for Linux

---
+++ mc-sema/tests/demo_sailboat.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source env.sh
+
+rm -f sailboat.o sailboat_mine.o sailboat.cfg sailboat.bc sailboat_opt.bc sailboat_run.exe
+
+${CC} -O2 -ggdb -m32 -c -o sailboat.o sailboat.c 
+
+if [ -e "${IDA_PATH}/idaq" ]
+then
+    echo "Using IDA to recover CFG"
+    ${BIN_DESCEND_PATH}/bin_descend_wrapper.py -func-map=sailboat.txt -entry-symbol=keycomp -i=sailboat.o
+else
+    echo "Using bin_descend to recover CFG"
+    ${BIN_DESCEND_PATH}/bin_descend -func-map=sailboat.txt -entry-symbol=keycomp -i=sailboat.o
+fi
+
+${CFG_TO_BC_PATH}/cfg_to_bc -i sailboat.cfg -driver=sailboat,keycomp,1,return,C -o sailboat.bc
+
+${LLVM_PATH}/opt -O3 -o sailboat_opt.bc sailboat.bc
+${LLVM_PATH}/llc -filetype=obj -o sailboat_mine.o sailboat_opt.bc
+${CC} -ggdb -m32 -o sailboat_run.exe sailboat_run.c sailboat_mine.o
+./sailboat_run.exe "key{d9dd1cb9dc13ebc3dc3780d76123ee34}"
