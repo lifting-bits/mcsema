bbb32594709b71105103ac3a24eee13b3f1a54e3
Artem Dinaburg <artem@trailofbits.com>
2014-10-26 21:26:51 -0700

    More progress on C++

--- mc-sema/tests/demo_cpp.sh
+++ mc-sema/tests/demo_cpp.sh
@@ -12,12 +12,13 @@ then
     ${BIN_DESCEND_PATH}/bin_descend_wrapper.py -d -func-map=linux_map.txt -i=demo_cpp -entry-symbol=main
 else
     echo "Using bin_descend to recover CFG"
-    ${BIN_DESCEND_PATH}/bin_descend -d -func-map=linux_map.txt -i=demo_cpp -entry-symbol=main
+    ${BIN_DESCEND_PATH}/bin_descend -ignore-native-entry-points -d -func-map=linux_map.txt -i=demo_cpp -entry-symbol=main
 fi
 
 ${CFG_TO_BC_PATH}/cfg_to_bc -i demo_cpp.cfg -driver=mcsema_main,main,2,return,C -o demo_cpp.bc
 
-#${LLVM_PATH}/opt -O3 -o demo_test3_opt.bc demo_test3.bc
-#${LLVM_PATH}/llc -filetype=obj -o demo_test3_mine.o demo_test3_opt.bc
-#${CC} -ggdb -m32 -o demo_driver3.exe demo_driver3.c demo_test3_mine.o
-#./demo_driver3.exe
+${LLVM_PATH}/opt -O3 -o demo_cpp_opt.bc demo_cpp.bc 
+${LLVM_PATH}/llc -filetype=obj -o demo_cpp.o demo_cpp_opt.bc
+
+${CXX} -m32 -ggdb -o demo_cpp.exe driver_cpp.cpp demo_cpp.o 
+./demo_cpp.exe
