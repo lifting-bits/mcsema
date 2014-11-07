932abfb68876564c80e3b4db3f2e2aa23c03acb4
Artem Dinaburg <artem@trailofbits.com>
2014-11-07 14:45:17 -0800

    Instruction Tests work on Linux

--- mc-sema/validator/valTool/valTool.cmake
+++ mc-sema/validator/valTool/valTool.cmake
@@ -6,4 +6,9 @@ execute_process(
 	COMMAND nmake -f Nmakefile
 	WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
 	)
+else(WIN32)
+execute_process(
+	COMMAND make -f makefile
+	WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+	)
 endif(WIN32)
