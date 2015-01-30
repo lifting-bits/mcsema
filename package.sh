564ee08ad117da78416cbfe0de94f8bdb4d65b68
Dan Guido <dan@trailofbits.com>
2015-01-30 17:50:38 -0500

    better packaging, closes #21

--- package.sh
+++ package.sh
@@ -1,49 +1,31 @@
 #!/bin/bash
 
-echo "This script packages mcsema into a .deb package"
-echo "Cleaning old diretories"
-rm -rf ./deb-build
+echo "This script packages mcsema into deb and rpm packages"
 
-GIT_DATE=$(git log -n 1 --format="%ai")
-DATE=$(date --utc --date="${GIT_DATE}" +%Y%m%d%H%M)
-VERSION=0.1-${DATE}
+echo "Cleaning old directory"
+rm -rf ./package
 
-
+# get a version number
+GIT_HASH=$(git rev-parse --short HEAD)
+VERSION=0.2-${GIT_HASH}
 echo "MCSEMA Version is: ${VERSION}"
 
-mkdir -p ./deb-build/mcsema_${VERSION}/opt/mcsema/bin/
-
-cp -R ./debian ./deb-build/mcsema_${VERSION}/DEBIAN
-echo "Version: ${VERSION}" | cat - ./debian/control > ./deb-build/mcsema_${VERSION}/DEBIAN/control
-
+# collect the mcsema bins
+mkdir package
 MC_BINS=$(find ./build/mc-sema -executable -type f)
-
 if [ "${MC_BINS}" == "" ]
 then
     echo "Could not find mcsema binaries. Did you build mcsema?"
     exit -1
 fi
-
 for BINFILE in ${MC_BINS}
 do
     echo "Packaging ${BINFILE}..."
-    cp ${BINFILE} ./deb-build/mcsema_${VERSION}/opt/mcsema/bin/
-done
-
-
-LLVM_BINS=$(find ./build/llvm-3.2 -executable -type f ! -name '*.so')
-if [ "${LLVM_BINS}" == "" ]
-then
-    echo "Could not find LLVM binaries. Did you build mcsema?"
-    exit -1
-fi
-
-for BINFILE in ${LLVM_BINS}
-do
-    echo "Packaging ${BINFILE}..."
-    cp ${BINFILE} ./deb-build/mcsema_${VERSION}/opt/mcsema/bin/
+    cp ${BINFILE} ./package/
 done
 
 echo "Building .deb file..."
-dpkg-deb -v --build ./deb-build/mcsema_${VERSION}
+fpm -s dir -t deb --name mcsema --version ${VERSION} --maintainer "<mcsema@trailofbits.com>" --url "https://github.com/trailofbits/mcsema --vendor "Trail of Bits --prefix "/usr/local/bin" -C ./package .
 
+echo "Building .rpm file..."
+fpm -s dir -t rpm --name mcsema --version ${VERSION} --maintainer "<mcsema@trailofbits.com>" --url "https://github.com/trailofbits/mcsema --vendor "Trail of Bits --prefix "/usr/local/bin" -C ./package .
