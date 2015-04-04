7bb2d3f3842a15f0d61f2e9488f0ac66dbb4bc24
Artem Dinaburg <artem@trailofbits.com>
2015-04-04 00:35:55 -0400

    Add new tests

--- mc-sema/validator/tests/CVTSS2SDrm.asm
+++ mc-sema/validator/tests/CVTSS2SDrm.asm
@@ -4,18 +4,14 @@ BITS 32
 ;TEST_IGNOREFLAGS=
 ;TEST_FILE_META_END
 
-; put 2 into ecx for future load into xmm0
-mov ecx, 2
-cvtsi2sd xmm0, ecx
-
 ;TEST_BEGIN_RECORDING
-lea ecx, [esp-8]
-movsd [ecx], xmm0
-cvtss2sd xmm1, [ecx]
-mov eax, 0
-mov ecx, 2
+lea ecx, [esp-4]
+mov DWORD [ecx], 0x3fc00000
+
+cvtss2sd xmm0, [ecx]
+
+mov ecx, [ecx]
 ;TEST_END_RECORDING
 
 xor ecx, ecx
 cvtsi2sd xmm0, ecx
-cvtsi2sd xmm1, ecx
