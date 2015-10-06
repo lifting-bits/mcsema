f78919f0bdac09eff9db6536d6c2dc797d40e19c
Artem Dinaburg <artem@trailofbits.com>
2015-10-06 18:37:02 -0400

    Support 64-bit cmov

--- mc-sema/cfgToLLVM/x86Instrs_CMOV.cpp
+++ mc-sema/cfgToLLVM/x86Instrs_CMOV.cpp
@@ -111,7 +111,9 @@ static InstTransResult  doCMOV(
         m[which ## 16rm] = EMIT_CMOV_RM(16, condition);\
         m[which ## 16rr] = EMIT_CMOV_RR(16, condition);\
         m[which ## 32rm] = EMIT_CMOV_RM(32, condition);\
-        m[which ## 32rr] = EMIT_CMOV_RR(32, condition);
+        m[which ## 32rr] = EMIT_CMOV_RR(32, condition);\
+        m[which ## 64rm] = EMIT_CMOV_RM(32, condition);\
+        m[which ## 64rr] = EMIT_CMOV_RR(32, condition);
 
 void CMOV_populateDispatchMap(DispatchMap &m) {
         EMIT_CMOV(X86::CMOVA,   AND(b, NOT(b, CF), NOT(b, ZF)));
