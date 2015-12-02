f0824813473a3303fbcdac4973a6adb02c1b6c9c
Peter Goodman <peter.goodman@gmail.com>
2015-12-02 17:18:53 -0500

    Making stack size bigger, passing envp.

--- drivers/ELF_64_linux.cpp
+++ drivers/ELF_64_linux.cpp
@@ -6,17 +6,17 @@
 #include "mc-sema/common/RegisterState.h"
 
 struct alignas(128) Stack {
-  char data[1UL << 20U];
+  char data[1UL << 21U];
 };
 
 static __thread mcsema::RegState tRegState;
 static __thread Stack tStack;
 
 extern "C" int mcsema_main(mcsema::RegState *);
-
-int main(int argc, char **argv) {
+extern "C" int main(int argc, char **argv, char **envp) {
   tRegState.RDI = argc;
   tRegState.RSI = reinterpret_cast<uintptr_t>(argv);
+  tRegState.RDX = reinterpret_cast<uintptr_t>(envp);
   tRegState.RSP = reinterpret_cast<uintptr_t>(&tStack + 1);
   mcsema_main(&tRegState);
   return static_cast<int>(tRegState.RAX);
