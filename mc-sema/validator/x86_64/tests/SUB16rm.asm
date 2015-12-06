a46d4ccbe5e3b99240ca9b94900ec8713a2af495
Artem Dinaburg <artem@trailofbits.com>
2015-12-05 23:35:14 -0500

    Fix this test for 64-bit so the upper 64-bits aren't different

--- mc-sema/validator/x86_64/tests/SUB16rm.asm
+++ mc-sema/validator/x86_64/tests/SUB16rm.asm
@@ -7,8 +7,8 @@ BITS 64
     ;TEST_BEGIN_RECORDING
     lea rcx, [rsp-0x4]
     mov DWORD [rcx], 0xc8
-    mov edx, 0x3
+    mov rdx, 0x3
     sub dx, WORD [rcx]
-    mov rcx, [rcx]
+    mov edx, DWORD [rcx]
     ;TEST_END_RECORDING
 
