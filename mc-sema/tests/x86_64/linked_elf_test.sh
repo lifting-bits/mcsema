eb44a2f13d76139a8c31d54d4ebfc628a003f446
Artem Dinaburg <artem@trailofbits.com>
2016-02-19 16:55:18 -0500

    Remove linux_map from local tests since its subsumed by the default linux std_defs

--- mc-sema/tests/x86_64/linked_elf_test.sh
+++ mc-sema/tests/x86_64/linked_elf_test.sh
@@ -9,10 +9,10 @@ ${CC} -ggdb -m64 -o linked_elf linked_elf.c
 if [ -e "${IDA_PATH}/idaq" ]
 then
     echo "Using IDA to recover CFG"
-    ${BIN_DESCEND_PATH}/bin_descend_wrapper.py -march=x86-64 -d -func-map=linux_map.txt -i=linked_elf -entry-symbol=main >> /dev/null
+    ${BIN_DESCEND_PATH}/bin_descend_wrapper.py -march=x86-64 -d -func-map=${STD_DEFS} -i=linked_elf -entry-symbol=main >> /dev/null
 else
     echo "Using bin_descend to recover CFG"
-    ${BIN_DESCEND_PATH}/bin_descend -d -march=x86-64 -func-map=linux_map.txt -i=linked_elf -entry-symbol=main
+    ${BIN_DESCEND_PATH}/bin_descend -d -march=x86-64 -func-map=${STD_DEFS} -i=linked_elf -entry-symbol=main
 fi
 
 ${CFG_TO_BC_PATH}/cfg_to_bc -mtriple=x86_64-pc-linux-gnu -i linked_elf.cfg -driver=mcsema_main,main,2,return,C -o linked_elf.bc
