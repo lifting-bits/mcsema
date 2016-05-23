c50667d1e1468dd44ee878a46146b7829fe79a93
Artem Dinaburg <artem@trailofbits.com>
2016-05-23 19:09:42 -0400

    Link callback stub to demo13

--- mc-sema/tests/x86_64/demo13.sh
+++ mc-sema/tests/x86_64/demo13.sh
@@ -19,7 +19,9 @@ fi
 
 ${CFG_TO_BC_PATH}/cfg_to_bc -mtriple=x86_64-pc-linux-gnu -i demo_test13.cfg -driver=demo13_entry,switches,1,return,C -o demo_test13.bc
 
-${LLVM_PATH}/opt -O3 -o demo_test13_opt.bc demo_test13.bc
+
+${LLVM_PATH}/llvm-link demo_test13.bc ${RUNTIME_PATH}/linux_amd64_callback.bc > demo_test13_linked.bc
+${LLVM_PATH}/opt -O3 -o demo_test13_opt.bc demo_test13_linked.bc
 ${LLVM_PATH}/llc -march=x86-64 -filetype=obj -o demo_test13_mine.o demo_test13_opt.bc
 ${LLVM_PATH}/llc -march=x86-64 -filetype=asm -o demo_test13_mine.asm demo_test13_opt.bc
 ${CC} -ggdb -m64 -o demo_driver13.exe demo_driver13.c demo_test13_mine.o
