d5aa6788ab7a38b854b6edb0a027d750eec7d801
Artem Dinaburg <artem@trailofbits.com>
2016-05-27 23:44:25 -0400

    maybe working do_call_value...

--- mc-sema/runtime/linux_i386_callback.c
+++ mc-sema/runtime/linux_i386_callback.c
@@ -160,16 +160,16 @@ void do_call_value(void *state, uint32_t value)
     //uint32_t reg_state[] = cs->reg_state;
 
     __asm__ volatile(
-            "pusha\n"
+            "pusha\n" // save all regs just so we don't have bother keeping track of what we saved
             "fxsave %0\n" // save sse state
-            "movl %3, %%eax\n"  // "r"(state)
-            "movl %4, %%ecx\n"  // "r"(value)"
-            "movl %2, %%esi\n" // "m"(cs)
+            "movl %3, %%eax\n"  // capture "state" arg (mcsema regstate)
+            "movl %4, %%ecx\n"  // capture "value" arg (call destination)
+            "movl %2, %%esi\n" // pointer to TLS area where we save state
             "movl %c[state_edi](%%eax), %%edi\n" // dump struct regs to state
             "movl %c[state_edx](%%eax), %%edx\n"
             "movl %c[state_ebx](%%eax), %%ebx\n"
             "movl %c[state_ebp](%%eax), %%ebp\n"
-            "movups %c[state_xmm0](%%eax), %%xmm0\n"
+            "movups %c[state_xmm0](%%eax), %%xmm0\n" // dump struct regs xmm state
             "movups %c[state_xmm1](%%eax), %%xmm1\n"
             "movups %c[state_xmm2](%%eax), %%xmm2\n"
             "movups %c[state_xmm3](%%eax), %%xmm3\n"
@@ -178,32 +178,49 @@ void do_call_value(void *state, uint32_t value)
             "movups %c[state_xmm6](%%eax), %%xmm6\n"
             "movups %c[state_xmm7](%%eax), %%xmm7\n"
             "leal %c[real_esp_off](%%esi), %%esi\n" // where will we save the "real" esp?
-            "movl %%esp, (%%esi)\n" // save esp
-            "movl %c[state_esp](%%eax), %%esp\n" // switch stacks
+            "movl %%esp, (%%esi)\n" // save our esp since we will switch to mcsema esp later
+            "movl %c[state_esp](%%eax), %%esp\n" // switch to mcsema stack
             "addl $4, %%esp\n" // we pushed a fake return addr before, undo it
-            "movl %%ecx, -4(%%esp)\n" // use that slot (since we know its valid)
-//            "pushl 0f\n" // get start of addrs
-            "movl %c[jmp_count](%%esi), %%ecx\n"
-            "imull $7, %%ecx, %%ecx\n" //find correct offset to return to to
-            "movl 0f, %%esi\n"
-            "addl %%ecx, %%esi\n" // add offset to ret addr location
+            "movl %%ecx, -4(%%esp)\n" // use that slot to store jump destination
+            "movl %c[jmp_count](%%esi), %%ecx\n" // save recursion count into ecx
+            "imull $7, %%ecx, %%ecx\n" //use that to calc how many INC instructions to skip
+            "movl 0f, %%esi\n" // base return addr
+            "addl %%ecx, %%esi\n" // calculate return addr
             "pushl %%esi\n" // push return addr
             "movl %c[state_ecx](%%eax), %%ecx\n" // complete struct regs spill
             "movl %c[state_esi](%%eax), %%esi\n"
             "movl %c[state_eax](%%eax), %%eax\n"
             "jmpl *-4(%%esp)\n"
             "0:\n"
+            "incl %1\n" // set of jump locations that increment the call frame counter
+            "incl %1\n" // the amount of these hit depends on the recursion depth
+            "incl %1\n" // at depth 0, none are hit, at depth 1, there is 1, etc.
             "incl %1\n"
             "incl %1\n"
             "incl %1\n"
             "incl %1\n"
-            "incl %1\n"
-            "incl %1\n"
-            "incl %1\n"
-            "pushl %%eax\n"
-            "movl %1, %%eax\n"
-            "imull %c[struct_size], %%eax\n"
-            "addl %5, %%eax\n"
+            "pushl %%eax\n" // save return value
+            "pushl %%esi\n" // save temp reg
+            "movl %1, %%eax\n" // get our recursion depth
+            "imull %c[struct_size], %%eax\n" // see where we need to index into the save state array
+            "addl %5, %%eax\n" // eax now points to our old saved state
+            "leal %c[reg_state](%%eax), %%esi\n" // get reg state offset
+            "movl %%ebx, %c[state_ebx](%%esi)\n" // convert native state to struct regs
+            "movl %%ecx, %c[state_ecx](%%esi)\n" // convert native state to struct regs
+            "movl %%edx, %c[state_edx](%%esi)\n" // convert native state to struct regs
+            "movl %%edi, %c[state_edi](%%esi)\n" // convert native state to struct regs
+            "movl %%ebp, %c[state_ebp](%%esi)\n" // convert native state to struct regs
+            "movl %%eax, %%ebx\n" // already saved ebx, so lets use it as temp reg
+            "movl %%esi, %%ecx\n" // already saved ecx, so lets use it as temp reg
+            "popl %%esi\n" // get esi from function return
+            "popl %%eax\n" // get eax from function return
+            "movl %%eax, %c[state_eax](%%ecx)\n" // convert native state to struct regs
+            "movl %%esi, %c[state_esi](%%ecx)\n" // convert native state to struct regs
+            "movl %%esp, %c[state_esp](%%ecx)\n" // convert native state to struct regs
+            "leal %c[real_esp_off](%%ebx), %%esi\n" // location of old native esp
+            "movl (%%esi), %%esp\n" // return original stack
+            "fxrstor %0\n"
+            "popa\n"
             : "=m"(cs->sse_state), "=m"(call_frame_counter)
             : "m"(cs), "m"(state), "m"(value), "m"(do_call_state[0]),
                     [state_eax]"e"(offsetof(RegState, EAX)),
@@ -224,6 +241,8 @@ void do_call_value(void *state, uint32_t value)
                     [state_xmm7]"e"(offsetof(RegState, XMM7)),
                     [real_esp_off]"e"(offsetof(do_call_state_t, __mcsema_real_esp)),
                     [jmp_count]"e"(offsetof(do_call_state_t, __mcsema_jmp_count)),
+                    [reg_state]"e"(offsetof(do_call_state_t, reg_state)),
+                    [sse_state]"e"(offsetof(do_call_state_t, sse_state)),
                     [struct_size]"e"(sizeof(do_call_state_t))
             : "memory", "eax", "ecx", "esi" );
     
