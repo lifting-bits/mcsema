af962613847a7442c8b200b33fdbd58afc683f85
Artem Dinaburg <artem@trailofbits.com>
2016-06-01 18:44:20 -0400

    fix demo13 since it uses callbacks

--- mc-sema/tests/demo_driver13.c
+++ mc-sema/tests/demo_driver13.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
 extern int demo13_entry(int);
+extern void* __mcsema_create_alt_stack(size_t stack_size);
+extern void* __mcsema_free_alt_stack(size_t stack_size);
 
 int main(int argc, char *argv[]) {
+    __mcsema_create_alt_stack(4096*2);
+
     int i = 0;
 
     for(i = 0; i <= 255; i++) {
         demo13_entry(i);
     }
 
+    __mcsema_free_alt_stack(4096*2);
     return 0;
 }
