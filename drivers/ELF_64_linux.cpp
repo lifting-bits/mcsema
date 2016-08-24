2c52df2bb169c0dde7efb81d4d7205ae5349f7d8
Artem Dinaburg <artem@trailofbits.com>
2016-08-24 18:36:35 -0400

    Updating drivers to use __mcsema_init

--- drivers/ELF_64_linux.cpp
+++ drivers/ELF_64_linux.cpp
@@ -13,11 +13,13 @@ static __thread mcsema::RegState tRegState;
 static __thread Stack tStack;
 
 extern "C" int mcsema_main(mcsema::RegState *);
+extern "C" void __mcsema_init(void);
 extern "C" int main(int argc, char **argv, char **envp) {
   tRegState.RDI = argc;
   tRegState.RSI = reinterpret_cast<uintptr_t>(argv);
   tRegState.RDX = reinterpret_cast<uintptr_t>(envp);
   tRegState.RSP = reinterpret_cast<uintptr_t>(&tStack + 1);
+  __mcsema_init();
   mcsema_main(&tRegState);
   return static_cast<int>(tRegState.RAX);
 }
