436c346cc3955c977134fce526bbee7da41df9ad
Garret Reece <garret@trailofbits.com>
2016-08-30 14:20:57 -0500

    stack vars track instructions that write to them, and pointers to stack vars track which stack var they point to

--- mc-sema/bin_descend/var_recovery/collect_ida.py
+++ mc-sema/bin_descend/var_recovery/collect_ida.py
@@ -52,7 +52,7 @@ def _get_flags_from_bits(flag):
       524288:'FF_VAR',
       49152:'FF_ANYNAME',
     }
- 
+
     _0type = {
       'MASK':15728640,
       1048576:'FF_0NUMH',
@@ -99,7 +99,7 @@ def _get_flags_from_bits(flag):
 
     output = ""
     output = cls[cls['MASK']&flag]
-    
+
     for category in [comm, _0type, _1type, datatype]:
         #the ida docs define, for example, a FF_0VOID = 0 constant in with the rest
         #  of the 0type constants, but I _think_ that just means
@@ -109,7 +109,7 @@ def _get_flags_from_bits(flag):
             output = output + " | " + val
     return output
 
-def collect_func_vars(F):
+def collect_func_vars(f):
     '''
     Collect stack variable data from a single function F.
     Returns a dict of stack variables 'stackArgs'.
@@ -117,28 +117,27 @@ def collect_func_vars(F):
     variable_flags is a string with flag names.
     '''
     stackArgs = dict()
-    
-    f = F.entry_address
+
     name = idc.Name(f)
     end = idc.GetFunctionAttr(f, idc.FUNCATTR_END)
     _locals = idc.GetFunctionAttr(f, idc.FUNCATTR_FRSIZE)
     frame = idc.GetFrame(f)
     if frame is None:
         return stackArgs
-    
+
     #grab the offset of the stored frame pointer, so that
-    #we can correlate offsets correctly in referant code
+    #we can correlate offsets correctly in referent code
     # e.g., EBP+(-0x4) will match up to the -0x4 offset
     delta = idc.GetMemberOffset(frame, " s")
     if -1 == delta:
-        #indicates that it wasn't found. Unsure exactly what to do 
+        #indicates that it wasn't found. Unsure exactly what to do
         # in that case, punting for now
         delta = 0
-    
+
     offset = idc.GetFirstMember(frame)
     while -1 != _signed_from_unsigned(offset):
         memberName = idc.GetMemberName(frame, offset)
-        if memberName is None: 
+        if memberName is None:
             # gaps in stack usage are fine, but generate trash output
             # gaps also could indicate a buffer that IDA doesn't recognize
             offset = idc.GetStrucNextOff(frame, offset)
@@ -151,12 +150,12 @@ def collect_func_vars(F):
         memberFlag = idc.GetMemberFlag(frame, offset)
         #TODO: handle the case where a struct is encountered (FF_STRU flag)
         flag_str = _get_flags_from_bits(memberFlag)
-        stackArgs[offset-delta] = {"name":memberName, "size":memberSize, "flags":flag_str, "instructions":set()}
+        stackArgs[offset-delta] = {"name":memberName, "size":memberSize, "flags":flag_str, "instructions":set(), "referent":set()}
         offset = idc.GetStrucNextOff(frame, offset)
     #functions.append({"name":name, "stackArgs":stackArgs})
     if len(stackArgs) > 0:
       _find_local_references(f, {"name":name, "stackArgs":stackArgs})
-    
+
     return stackArgs
 
 def collect_func_vars_all():
@@ -174,7 +173,7 @@ def collect_func_vars_all():
         #name = idc.Name(f)
         f_ea = idc.GetFunctionAttr(f, idc.FUNCATTR_START)
         f_vars = collect_func_vars(f)
-        functions.append({"ea":f_ea, "stackArgs":f_vars})
+        functions.append({"ea":f_ea, "stackArgs":f_vars, "name":idc.Name(f)})
     return functions
 
 def _process_mov_inst(addr, referers, dereferences, func_var_data):
@@ -187,30 +186,38 @@ def _process_mov_inst(addr, referers, dereferences, func_var_data):
     '''
     base_ptr_format = "[{}+".format(_base_ptr)
     stack_ptr_format = "[{}+".format(_stack_ptr)
-    referers.discard(idc.GetOpnd(addr, 0))
+
+    #remove the target operand from the taint collections
+    referers.pop(idc.GetOpnd(addr, 0), None)
     dereferences.pop(idc.GetOpnd(addr, 0), None)
 
     target_op = idc.GetOpnd(addr, 0)
 
+
     if idc.GetOpnd(addr, 1) in referers:
+        # handling the two following mov cases in this block:
+        #   lea eax, [ebp-8]  #collecting the address of a stack variable
+        #   mov ebx, eax  # copying referent data around
+        #   mov [ebp-4], ebx   # copying referent data into a stack variable (i.e., moving an address into a pointer)
+
         if base_ptr_format in target_op:
-            target_offset = target_op[len(base_ptr_format):target_op.index(']')]
+            #moving referent data into a stack variable (i.e., moving an address into a pointer)
             offset = _signed_from_unsigned(idc.GetOperandValue(addr, 0))
             if offset in func_var_data["stackArgs"].keys():
                 func_var_data["stackArgs"][offset]["flags"] += " | LOCAL_REFERER"
+                # collect which stack variable offset this variable points to
+                func_var_data["stackArgs"][offset]["referent"].add(referers[idc.GetOpnd(addr, 1)])
         else:
-            # lea eax, [ebp-8]
-            # mov ebx, eax
-            # mov [ebp-4], ebx
-            referers.add(target_op)
+            #moving referent data around
+            referers[target_op] = referers[idc.GetOpnd(addr, 1)]
     elif base_ptr_format in idc.GetOpnd(addr, 1) or stack_ptr_format in idc.GetOpnd(addr, 1):
+        # mov eax, [ebp-4]  //eax now tainted with stack var data
         offset = _signed_from_unsigned(idc.GetOperandValue(addr, 1))
         if offset in func_var_data["stackArgs"].keys():
             dereferences[idc.GetOpnd(addr, 0)] = offset
 
     ''' collect EAs of instructions that write to stack variables'''
-    if base_ptr_format in target_op:
-        target_offset = target_op[len(base_ptr_format):target_op.index(']')]
+    if base_ptr_format in target_op or stack_ptr_format in target_op:
         offset = _signed_from_unsigned(idc.GetOperandValue(addr, 0))
         if offset in func_var_data["stackArgs"].keys():
             func_var_data["stackArgs"][offset]["instructions"].add(addr)
@@ -223,38 +230,38 @@ def _find_local_references(func, func_var_data):
     frame = idc.GetFrame(func)
     if frame is None:
         return
-    regs = dict()
-    referers = set() # members of this set contain the address of an element on the stack
+    referers = dict() # members of this collection contain the address of an element on the stack. keys are operands, values are stack offset
     dereferences = dict() # members of this collection contain the data of an element on the stack
     for addr in idautils.FuncItems(func):
         if "lea" == idc.GetMnem(addr):
             if base_ptr_format in idc.GetOpnd(addr, 1) or stack_ptr_format in idc.GetOpnd(addr, 1):
-                # right now just capture that it's a local reference, not its referant
-                referers.add(idc.GetOpnd(addr, 0))
+                #referers[operand] = offset
+                referers[idc.GetOpnd(addr, 0)] = _signed_from_unsigned(idc.GetOperandValue(addr, 1))
         if "mov" == idc.GetMnem(addr):
             _process_mov_inst(addr, referers, dereferences, func_var_data)
         if "call" == idc.GetMnem(addr):
             target_op = idc.GetOpnd(addr, 0)
             if target_op in referers:
+                #maybe do something here? explicitly calling to the stack. Hrm.
                 pass
             if target_op in dereferences:
                 # mov eax, [ebp+4]
                 # call eax
-                func_var_data["stackArgs"][dereferences[target_op]][2] += " | CODE_PTR"
+                func_var_data["stackArgs"][dereferences[target_op]]["flags"] += " | CODE_PTR"
             # clear the referers and dereferences?
 
-
-if __name__ == "__main__":
-    print_func_vars()
 
 def print_func_vars():
     print
     print "Stack Vars:"
-    func_list = collect_func_vars()
+    func_list = collect_func_vars_all()
     for entry in func_list:
         print "{} {{".format(entry['name'])
-        for var in entry['stackArgs']:
-            print "  {}".format(var)
+        for offset in sorted(entry['stackArgs'].keys()):
+            print "  {}: {}".format(hex(offset), entry['stackArgs'][offset])
     print "}"
     print
     print "End Stack Vars"
+
+if __name__ == "__main__":
+    print_func_vars()
