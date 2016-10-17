00d0b6fe60b81308f58767714e173e39bd5a5c0a
Artem Dinaburg <artem@trailofbits.com>
2016-10-17 17:02:07 -0400

    linked elf test works

--- mc-sema/tests/x86_64/linked_elf_test.sh
+++ mc-sema/tests/x86_64/linked_elf_test.sh
@@ -11,14 +11,11 @@ then
     echo "Using IDA to recover CFG"
     ${BIN_DESCEND_PATH}/bin_descend_wrapper.py -march=x86-64 -d -func-map=${STD_DEFS} -i=linked_elf -entry-symbol=main >> /dev/null
 else
-    echo "Using bin_descend to recover CFG"
-    ${BIN_DESCEND_PATH}/bin_descend -d -march=x86-64 -func-map=${STD_DEFS} -i=linked_elf -entry-symbol=main
+    echo "Please install IDA to recover the control flow graph; bin_descend is now deprecated"
+    exit 1
 fi
 
-${CFG_TO_BC_PATH}/cfg_to_bc -mtriple=x86_64-pc-linux-gnu -i linked_elf.cfg -driver=mcsema_main,main,2,return,C -o linked_elf.bc
-${LLVM_PATH}/opt -O3 -o linked_elf_opt.bc linked_elf.bc
-${LLVM_PATH}/llc -filetype=obj -o linked_elf.o linked_elf_opt.bc
-
-${CC} -m64 -ggdb -o linked_elf_out.exe driver_linked_elf.c linked_elf.o
+${CFG_TO_BC_PATH}/cfg_to_bc -mtriple=x86_64-pc-linux-gnu -i linked_elf.cfg -entrypoint=main -o linked_elf.bc
+clang-3.5 -O3 -m64 -o linked_elf_out.exe ../../../drivers/ELF_64_linux.S linked_elf.bc
 
 ./linked_elf_out.exe
