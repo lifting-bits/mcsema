af1b2ceacbe193296db35e3d20878fea03396d2d
Artem Dinaburg <artem@trailofbits.com>
2016-10-17 16:59:44 -0400

    remove maze driver; fix qsort demo

--- mc-sema/tests/x86_64/demo_qsort.sh
+++ mc-sema/tests/x86_64/demo_qsort.sh
@@ -11,15 +11,11 @@ then
     echo "Using IDA to recover CFG"
     ${BIN_DESCEND_PATH}/bin_descend_wrapper.py -march=x86-64 -func-map="qsort_map.txt" -entry-symbol=print_it -i=demo_qsort.o >> /dev/null
 else
-    echo "Using bin_descend to recover CFG"
-    ${BIN_DESCEND_PATH}/bin_descend -march=x86-64 -d -func-map="qsort_map.txt" -entry-symbol=print_it -i=demo_qsort.o
+    echo "Please install IDA to recover the control flow graph; bin_descend is now deprecated"
+    exit 1
 fi
 
-${CFG_TO_BC_PATH}/cfg_to_bc -mtriple=x86_64-pc-linux-gnu -i demo_qsort.cfg -driver=qsort_entry,print_it,raw,return,C -o demo_qsort.bc
+${CFG_TO_BC_PATH}/cfg_to_bc -mtriple=x86_64-pc-linux-gnu -i demo_qsort.cfg -entrypoint=print_it -o demo_qsort.bc
+clang-3.5 -O3 -m64 -o demo_driver_qsort.exe demo_driver_qsort.c ../../../drivers/ELF_64_linux.S demo_qsort.bc
 
-${LLVM_PATH}/opt -O3 -o demo_qsort_opt.bc demo_qsort.bc
-${LLVM_PATH}/llvm-link ${RUNTIME_PATH}/linux_amd64_callback.bc demo_qsort_opt.bc > demo_qsort_linked.bc
-${LLVM_PATH}/llc -march=x86-64 -filetype=obj -o demo_qsort_mine.o demo_qsort_linked.bc
-${CC} -ggdb -m64 -o demo_driver_qsort.exe demo_driver_qsort.c demo_qsort_mine.o
 ./demo_driver_qsort.exe
-
