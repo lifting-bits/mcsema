a430ba4709588727226ac1539fae7ce7dcd98a09
Artem Dinaburg <artem@trailofbits.com>
2016-10-20 13:33:18 -0400

    demo8 works

--- mc-sema/tests/demo8.sh
+++ mc-sema/tests/demo8.sh
@@ -10,17 +10,13 @@ ${CC} -ggdb -m32 -c -o demo_test8.o demo_test8.c
 if [ -e "${IDA_PATH}/idaq" ]
 then
     echo "Using IDA to recover CFG"
-    export IDALOG=logfile_demo8.txt
-    rm -f ${IDALOG}
-    ${BIN_DESCEND_PATH}/bin_descend_wrapper.py -d -march=x86 -entry-symbol=doOp -i=demo_test8.o >> ${IDALOG}
+    ${BIN_DESCEND_PATH}/bin_descend_wrapper.py -d -march=x86 -entry-symbol=doOp -i=demo_test8.o >> /dev/null
 else
-    echo "Using bin_descend to recover CFG"
-    ${BIN_DESCEND_PATH}/bin_descend -march=x86 -d -entry-symbol=doOp -i=demo_test8.o
+    echo "Please install IDA to recover the control flow graph; bin_descend is now deprecated"
+    exit 1
 fi
 
-${CFG_TO_BC_PATH}/cfg_to_bc -mtriple=i686-pc-linux-gnu -i demo_test8.cfg -driver=demo8_entry,doOp,1,return,C -o demo_test8.bc
+${CFG_TO_BC_PATH}/cfg_to_bc -mtriple=i686-pc-linux-gnu -i demo_test8.cfg -entrypoint=doOp -o demo_test8.bc
+clang-3.5 -O3 -m32 -o demo_driver8.exe demo_driver8.c ../../drivers/ELF_32_linux.S demo_test8.bc
 
-${LLVM_PATH}/opt -O3 -o demo_test8_opt.bc demo_test8.bc
-${LLVM_PATH}/llc -filetype=obj -o demo_test8_mine.o demo_test8_opt.bc
-${CC} -ggdb -m32 -o demo_driver8.exe demo_driver8.c demo_test8_mine.o
 ./demo_driver8.exe
