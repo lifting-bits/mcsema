37f8104f043d37873781356b9a92913056a6d53d
Riccardo Schirone <sirmy15@gmail.com>
2016-10-20 20:37:51 +0000

    mc-sema: fix Pin download on cmake 2.8
    cmake 2.8 does not support URL_HASH, only URL_MD5.

--- mc-sema/cmake/modules/FindPin.cmake
+++ mc-sema/cmake/modules/FindPin.cmake
@@ -1,6 +1,6 @@
 set(_PIN_NAMES pin)
 
-find_program(PIN_EXECUTABLE 
+find_program(PIN_EXECUTABLE
 	NAMES ${_PIN_NAMES}
 	PATHS "$ENV{PIN_HOME}" "${CMAKE_SOURCE_DIR}/thirdparty/win32/pin/ia32/bin" "C:/pin-2.10-45467-msvc10-ia32_intel64-windows/ia32/bin" "C:/pin/ia32/bin" "/usr/bin" "/opt/bin" "/usr/local/bin" "/bin" "/opt/pin"
 	)
