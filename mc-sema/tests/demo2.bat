945ee614818579b57b1fa76124482553664cc55f
Artem Dinaburg <artem@trailofbits.com>
2016-11-01 18:25:11 -0400

    Win32 demo2

--- mc-sema/tests/demo2.bat
+++ mc-sema/tests/demo2.bat
@@ -6,15 +6,13 @@ del /q demo_test2.cfg demo_driver2.obj demo_test2.obj demo_test2_mine.obj demo_d
 
 if exist "%IDA_PATH%\idaq.exe" (
     echo Using IDA to recover CFG
-    %BIN_DESCEND_PATH%\bin_descend_wrapper.py -d -march=x86 -entry-symbol=start -i=demo_test2.obj
+    %PYTHON% %BIN_DESCEND_PATH%\bin_descend_wrapper.py -d -march=x86 -entry-symbol=start -i=demo_test2.obj
 ) else (
-    echo Using bin_descend to recover CFG
-    %BIN_DESCEND_PATH%\bin_descend.exe -d -march=x86 -entry-symbol=start -i=demo_test2.obj
+    echo Bin_descend is no longer supported
+    exit 1
 )
 
-%CFG_TO_BC_PATH%\cfg_to_bc.exe -mtriple=i386-pc-win32 -i demo_test2.cfg -driver=demo2_entry,start,raw,return,C -o demo_test2.bc
+%CFG_TO_BC_PATH%\cfg_to_bc.exe -mtriple=i386-pc-win32 -i demo_test2.cfg -entrypoint=start -o demo_test2.bc
+clang -O3 -m32 -o demo_driver2.exe demo_driver2.c ..\..\drivers\PE_32_windows.asm demo_test2.bc
 
-%LLVM_PATH%\opt.exe -O3 -o demo_test2_opt.bc demo_test2.bc
-%LLVM_PATH%\llc.exe -filetype=obj -o demo_test2_mine.obj demo_test2_opt.bc
-"%VCINSTALLDIR%\bin\cl.exe" /Zi /nologo demo_driver2.c demo_test2_mine.obj
 demo_driver2.exe
