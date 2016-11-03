2ca641ad9891820f166d67ec4b1f87d82ee1902e
Artem Dinaburg <artem@trailofbits.com>
2016-11-03 15:16:27 -0400

    Win32 demo15

--- mc-sema/tests/demo15.bat
+++ mc-sema/tests/demo15.bat
@@ -6,15 +6,16 @@ cl /nologo /c demo_test15.c
 
 if exist "%IDA_PATH%\idaq.exe" (
     echo Using IDA to recover CFG
-    %BIN_DESCEND_PATH%\bin_descend_wrapper.py -march=x86 -d -func-map=%STD_DEFS% -entry-symbol=_imcdecl,_imstdcall@8,@imfastcall@8 -i=demo_test15.obj
+    %PYTHON% %BIN_DESCEND_PATH%\bin_descend_wrapper.py -march=x86 -d -func-map=%STD_DEFS% -entry-symbol=_imcdecl,_imstdcall@8,@imfastcall@8 -i=demo_test15.obj
 ) else (
-    echo Using bin_descend to recover CFG
-    %BIN_DESCEND_PATH%\bin_descend.exe -march=x86 -d -func-map=%STD_DEFS% -entry-symbol=_imcdecl,_imstdcall@8,@imfastcall@8 -i=demo_test15.obj
+    echo Bin_descend is no longer supported
+    exit 1
 )
 
-%CFG_TO_BC_PATH%\cfg_to_bc.exe -mtriple=i386-pc-win32 -i demo_test15.cfg -driver=imcdecl,_imcdecl,2,return,C -driver=imstdcall,_imstdcall@8,2,return,E -driver=imfastcall,@imfastcall@8,2,return,F -o demo_test15.bc
+%CFG_TO_BC_PATH%\cfg_to_bc.exe -mtriple=i386-pc-win32 -i demo_test15.cfg -entrypoint=_imcdecl -entrypoint=_imstdcall@8 -entrypoint=@imfastcall@8 -o demo_test15.bc
 
-%LLVM_PATH%\opt.exe -O1 -o demo_test15_opt.bc demo_test15.bc
-%LLVM_PATH%\llc.exe -filetype=obj -o demo_test15_lifted.obj demo_test15_opt.bc
-"%VCINSTALLDIR%\bin\cl.exe" /Zi /nologo demo_driver15.c demo_test15_lifted.obj
+clang -target i386-pc-win32 -O3 -m32 -c -o demo15_bc.obj demo_test15.bc
+clang -target i386-pc-win32 -O3 -m32 -c -o demo15_asm.obj ..\..\drivers\PE_32_windows.asm
+cl /Zi /nologo demo_driver15.c demo15_bc.obj demo15_asm.obj
+
 demo_driver15.exe
