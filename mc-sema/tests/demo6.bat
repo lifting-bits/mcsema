219b4d3a4293dba9d3ac487110f0d6d316362168
Artem Dinaburg <artem@trailofbits.com>
2016-11-03 12:30:28 -0400

    win32 demo6

--- mc-sema/tests/demo6.bat
+++ mc-sema/tests/demo6.bat
@@ -7,15 +7,16 @@ cl /nologo /c demo_test6.c
 
 if exist "%IDA_PATH%\idaq.exe" (
     echo Using IDA to recover CFG
-    %BIN_DESCEND_PATH%\bin_descend_wrapper.py -march=x86 -d -func-map="%STD_DEFS%" -entry-symbol=_doWork -i=demo_test6.obj
+    %PYTHON% %BIN_DESCEND_PATH%\bin_descend_wrapper.py -march=x86 -d -func-map="%STD_DEFS%" -entry-symbol=_doWork -i=demo_test6.obj
 ) else (
-    echo Using bin_descend to recover CFG
-    %BIN_DESCEND_PATH%\bin_descend.exe -march=x86 -d -func-map="%STD_DEFS%"  -entry-symbol=_doWork -i=demo_test6.obj
+    echo Bin_descend is no longer supported
+    exit 1
 )
 
-%CFG_TO_BC_PATH%\cfg_to_bc.exe -mtriple=i386-pc-win32 -i demo_test6.cfg -driver=demo6_entry,_doWork,2,return,C -o demo_test6.bc
+%CFG_TO_BC_PATH%\cfg_to_bc.exe -mtriple=i386-pc-win32 -i demo_test6.cfg -entrypoint=_doWork -o demo_test6.bc
 
-%LLVM_PATH%\opt.exe -O3 -o demo_test6_opt.bc demo_test6.bc
-%LLVM_PATH%\llc.exe -filetype=obj -o demo_test6_mine.obj demo_test6_opt.bc
-"%VCINSTALLDIR%\bin\cl.exe" /Zi /nologo demo_driver6.c demo_test6_mine.obj
+clang -target i386-pc-win32 -O3 -m32 -c -o demo6_bc.obj demo_test6.bc
+clang -target i386-pc-win32 -O3 -m32 -c -o demo6_asm.obj ..\..\drivers\PE_32_windows.asm
+cl /Zi /nologo demo_driver6.c demo6_bc.obj demo6_asm.obj
+
 demo_driver6.exe
