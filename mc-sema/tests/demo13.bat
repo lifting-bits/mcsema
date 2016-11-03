496311e4418d60717a913710b1592e707ee12560
Artem Dinaburg <artem@trailofbits.com>
2016-11-03 15:04:31 -0400

    Win32 demo13

--- mc-sema/tests/demo13.bat
+++ mc-sema/tests/demo13.bat
@@ -6,15 +6,15 @@ cl /nologo /c demo_test13.c
 
 if exist "%IDA_PATH%\idaq.exe" (
     echo Using IDA to recover CFG
-    %BIN_DESCEND_PATH%\bin_descend_wrapper.py -march=x86 -d -func-map=%STD_DEFS% -entry-symbol=_switches -i=demo_test13.obj
+    %PYTHON% %BIN_DESCEND_PATH%\bin_descend_wrapper.py -march=x86 -d -func-map=%STD_DEFS% -entry-symbol=_switches -i=demo_test13.obj
 ) else (
-    echo Using bin_descend to recover CFG
-    %BIN_DESCEND_PATH%\bin_descend.exe -march=x86 -d -func-map=%STD_DEFS% -entry-symbol=_switches -i=demo_test13.obj
+    echo Bin_descend is no longer supported
+    exit 1
 )
 
-%CFG_TO_BC_PATH%\cfg_to_bc.exe -mtriple=i386-pc-win32 -i demo_test13.cfg -driver=demo13_entry,_switches,1,return,C -o demo_test13.bc
+%CFG_TO_BC_PATH%\cfg_to_bc.exe -mtriple=i386-pc-win32 -i demo_test13.cfg -entrypoint=_switches -o demo_test13.bc
+clang -target i386-pc-win32 -O3 -m32 -c -o demo13_bc.obj demo_test13.bc
+clang -target i386-pc-win32 -O3 -m32 -c -o demo13_asm.obj ..\..\drivers\PE_32_windows.asm
+cl /Zi /nologo demo_driver13.c demo13_bc.obj demo13_asm.obj
 
-%LLVM_PATH%\opt.exe -O3 -o demo_test13_opt.bc demo_test13.bc
-%LLVM_PATH%\llc.exe -filetype=obj -o demo_test13_lifted.obj demo_test13_opt.bc
-"%VCINSTALLDIR%\bin\cl.exe" /Zi /nologo demo_driver13.c demo_test13_lifted.obj
 demo_driver13.exe
