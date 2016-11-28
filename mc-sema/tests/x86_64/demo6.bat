b0671eab724a2a2d7e93e1cd647ddb1951b4e9da
Artem Dinaburg <artem@trailofbits.com>
2016-11-28 13:19:27 -0800

    Win64 Demo6

--- mc-sema/tests/x86_64/demo6.bat
+++ mc-sema/tests/x86_64/demo6.bat
@@ -7,15 +7,12 @@ cl /nologo /c demo_test6.c
 
 if exist "%IDA_PATH%\idaq.exe" (
     echo Using IDA to recover CFG
-    %BIN_DESCEND_PATH%\bin_descend_wrapper.py -march=x86-64 -d -func-map="%STD_DEFS%" -entry-symbol=doWork -i=demo_test6.obj
+    %PYTHON% %BIN_DESCEND_PATH%\bin_descend_wrapper.py -march=x86-64 -d -func-map="%STD_DEFS%" -entry-symbol=doWork -i=demo_test6.obj
 ) else (
-    echo Using bin_descend to recover CFG
-    %BIN_DESCEND_PATH%\bin_descend.exe -march=x86-64 -d -func-map="%STD_DEFS%"  -entry-symbol=doWork -i=demo_test6.obj
+    echo Bin_descend is no longer supported
+    exit 1
 )
 
-%CFG_TO_BC_PATH%\cfg_to_bc.exe -mtriple=x86_64-pc-win32 -i demo_test6.cfg -driver=demo6_entry,doWork,2,return,C -o demo_test6.bc
-
-%LLVM_PATH%\opt.exe -O3 -o demo_test6_opt.bc demo_test6.bc
-%LLVM_PATH%\llc.exe -filetype=obj -o demo_test6_mine.obj demo_test6_opt.bc
-cl /Zi /nologo demo_driver6.c demo_test6_mine.obj
+%CFG_TO_BC_PATH%\cfg_to_bc.exe -mtriple=x86_64-pc-windows-msvc -i demo_test6.cfg -entrypoint=doWork -o demo_test6.bc
+clang-cl /Zi -O3 -m64 -o demo_driver6.exe demo_driver6.c ..\..\..\drivers\PE_64_windows.asm demo_test6.bc
 demo_driver6.exe
