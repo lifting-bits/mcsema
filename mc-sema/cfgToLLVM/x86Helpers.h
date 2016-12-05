c7fa6042aaf960da886924f2fc80ae1ed85fc6b1
car <hugin@mimisbrunnr.net>
2016-12-05 14:33:36 -0500

    namespacing fix

--- mc-sema/cfgToLLVM/x86Helpers.h
+++ mc-sema/cfgToLLVM/x86Helpers.h
@@ -51,8 +51,8 @@ llvm::Value *getValueForExternal(llvm::Module *M, InstPtr ip, llvm::BasicBlock *
           gvar->setDLLStorageClass(llvm::GlobalValue::DLLImportStorageClass);
           // sometimes windows will import this directly as the variable instead of
           // as a reference to a variable. But the rest of the code wants a pointer to var
-          llvm::Value *toPtr = new AllocaInst(gvar->getType(), "", block);
-          llvm::Value *writeIt = new StoreInst(gvar, toPtr, block);
+          llvm::Value *toPtr = new llvm::AllocaInst(gvar->getType(), "", block);
+          llvm::Value *writeIt = new llvm::StoreInst(gvar, toPtr, block);
           addrInt = new llvm::PtrToIntInst(
               toPtr, llvm::Type::getIntNTy(block->getContext(), width), "", block);
         } else {
