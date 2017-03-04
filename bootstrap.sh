9f19b625a1117a89e5092b55a5dfcc19dabf5bb5
Artem Dinaburg <artem@trailofbits.com>
2017-03-03 23:14:38 -0500

    Fix bootstrap of 32-bit generated assembly

--- bootstrap.sh
+++ bootstrap.sh
@@ -129,13 +129,13 @@ fi
 # Produce the runtimes.
 if [ ! -e ${GEN_DIR}/ELF_32_linux.S ]; then
   echo "[+] Generating runtimes"
-  clang++-3.8 -std=gnu++11 ${DIR}/mcsema/Arch/X86/print_ELF_32_linux.cpp
+  clang++-3.8 -m32 -std=gnu++11 ${DIR}/mcsema/Arch/X86/print_ELF_32_linux.cpp
   ./a.out > ${GEN_DIR}/ELF_32_linux.S
 
   clang++-3.8 -std=gnu++11 ${DIR}/mcsema/Arch/X86/print_ELF_64_linux.cpp
   ./a.out > ${GEN_DIR}/ELF_64_linux.S
 
-  clang++-3.8 -std=gnu++11 ${DIR}/mcsema/Arch/X86/print_PE_32_windows.cpp
+  clang++-3.8 -m32 -std=gnu++11 ${DIR}/mcsema/Arch/X86/print_PE_32_windows.cpp
   ./a.out > ${GEN_DIR}/PE_32_windows.asm
 
   clang++-3.8 -std=gnu++11 ${DIR}/mcsema/Arch/X86/print_PE_64_windows.cpp
