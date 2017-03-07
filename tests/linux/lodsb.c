a553008c61a7f88bcb96709c7bddefc7bfc10268
Artem Dinaburg <artem@trailofbits.com>
2017-03-06 20:02:57 -0500

    Add some scasb to the lodsb test

--- tests/linux/lodsb.c
+++ tests/linux/lodsb.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
+static inline int mystrcmp(const char *cs,const char *ct)
+{
+    register int __res;
+    __asm__ __volatile__(
+            "cld\n"
+            "1:\n"
+            "lodsb\n"
+            "scasb\n"
+            "jne 2f\n"
+            "testb %%al,%%al\n"
+            "jne 1b\n"
+            "xorl %%eax,%%eax\n"
+            "jmp 3f\n"
+            "2:\n"
+            "sbbl %%eax,%%eax\n"
+            "orb $1,%%al\n"
+            "3:\n"
+            :"=a" (__res):"S" (cs),"D" (ct):"si","di");
+    return __res;
+}
+
 static inline char *mystrcpy(char *dest, const char *src)
 {
     int d0, d1, d2;
@@ -24,4 +45,13 @@ int main(void)
         printf ("%c", buf[i]);
     printf("\n");
 
+    buf[13] = '\0';
+
+    if(mystrcmp("i am so cool.", buf) == 0) {
+        printf("The strings match\n");
+    } else {
+        printf("The strings do not match\n");
+    }
+
+    return 0;
 }
