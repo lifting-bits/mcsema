8b57bc7bf474632539b4305e2206a27a0e7ac3e4
Alessandro Gario <alessandrogario@users.noreply.github.com>
2017-05-02 04:22:36 +0200

    mcsema-lift: Added a -dumpcfg option to print the CFG contents to screen. (#252)
    * mcsema-lift: Added a -dumpcfg option to print the CFG contents to screen.
    
    * Style changes (see details).
    
     o Renamed --cfgdump to --cfg_dump
     o Moved the PrintVersion/DumpCFG functions in the anonymous
       namespace at the top of the file.
     o Only show lifted names when the native name is not available.
     o Deserialize the CFG file only once.

--- tools/mcsema_lift/Lift.cpp
+++ tools/mcsema_lift/Lift.cpp
@@ -20,6 +20,7 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <iomanip>
 
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
@@ -52,6 +53,7 @@ DEFINE_string(cfg, "", "Path to the CFG file containing code to lift.");
 DEFINE_string(output, "", "Output bitcode file name.");
 
 DECLARE_bool(version);
+DEFINE_bool(dump_cfg, false, "Dump the CFG contents to screen.");
 
 namespace {
 
@@ -60,6 +62,56 @@ static void PrintVersion(void) {
       << "This is mcsema-lift version: " << MCSEMA_VERSION_STRING << std::endl
       << "Built from branch: " << MCSEMA_BRANCH_NAME << std::endl
       << "Using LLVM " << LLVM_VERSION_STRING << std::endl;
+}
+
+static void DumpCFG(const mcsema::NativeModule *native_module) {
+  std::ios::fmtflags original_stream_flags(std::cout.flags());
+
+  // print the header on stderr so that the user can easily pipe the output to grep/awk
+  std::cerr << "Type Attr Address          Blocks   Instrs   Name\n";
+
+  for (const auto &pair : native_module->ea_to_func) {
+    std::uint64_t virtual_address = pair.first;
+    const auto *native_func = pair.second;
+
+    std::cout << "FUNC " << (native_func->is_external ? "EXT  " : "INT  ");
+    std::cout << std::hex << std::setfill('0') << std::setw(16) << virtual_address << " ";
+
+    if (native_func->is_external) {
+        std::cout << std::setfill(' ') << std::setw(8) << "NA" << " ";
+        std::cout << std::setfill(' ') << std::setw(8) << "NA" << " ";
+    } else {
+      std::size_t basic_block_count = 0;
+      std::size_t instruction_count = 0;
+
+      for (const auto &address_block_pair : native_func->blocks) {
+        ++basic_block_count;
+        instruction_count += address_block_pair.second->instructions.size();
+      }
+
+      std::cout << std::dec << std::setfill('0') << std::setw(8) << basic_block_count << " ";
+      std::cout << std::dec << std::setfill('0') << std::setw(8) << instruction_count << " ";
+    }
+
+    std::cout << (native_func->name.empty() ? native_func->lifted_name : native_func->name) << "\n";
+  }
+
+  for (const auto &pair : native_module->ea_to_var) {
+    std::uint64_t virtual_address = pair.first;
+    const auto &native_var = pair.second;
+
+    std::cout << "VAR  " << (native_var->is_external ? "EXT  " : "INT  ");
+    std::cout << std::hex << std::setfill('0') << std::setw(16) << virtual_address << " ";
+
+    std::cout << std::setfill(' ') << std::setw(8) << "NA" << " ";
+    std::cout << std::setfill(' ') << std::setw(8) << "NA" << " ";
+
+    std::cout << (native_var->name.empty() ? native_var->lifted_name : native_var->name) << "\n";
+  }
+  std::cout << std::endl;
+
+  // make sure we don't leave the stream in a bad state
+  std::cout.flags(original_stream_flags);
 }
 
 }  // namespace
@@ -72,6 +124,7 @@ int main(int argc, char *argv[]) {
      << "    --arch ARCH_NAME \\" << std::endl
      << "    --os OS_NAME \\" << std::endl
      << "    --cfg CFG_FILE \\" << std::endl
+     << "    [--dump_cfg] \\" << std::endl
      << "    [--version]" << std::endl
      << std::endl;
 
@@ -93,8 +146,8 @@ int main(int argc, char *argv[]) {
   CHECK(!FLAGS_cfg.empty())
       << "Must specify the path to a CFG file to --cfg.";
 
-  CHECK(!FLAGS_output.empty())
-      << "Must specify the lifted bitcode output path to --output.";
+  CHECK(FLAGS_dump_cfg && FLAGS_output.empty() || !FLAGS_dump_cfg && !FLAGS_output.empty())
+      << "Must either specify the bitcode output path (--output) or the --dump_cfg parameter.";
 
   mcsema::gContext = new llvm::LLVMContext;
 
@@ -102,11 +155,16 @@ int main(int argc, char *argv[]) {
       << "Cannot initialize for arch " << FLAGS_arch
       << " and OS " << FLAGS_os << std::endl;
 
+  auto cfg_module = mcsema::ReadProtoBuf(FLAGS_cfg);
+  if (FLAGS_dump_cfg) {
+    DumpCFG(cfg_module);
+    return EXIT_SUCCESS;
+  }
+
   auto input = remill::FindSemanticsBitcodeFile("", FLAGS_arch);
   mcsema::gModule = remill::LoadModuleFromFile(mcsema::gContext, input);
   mcsema::gArch->PrepareModule(mcsema::gModule);
 
-  auto cfg_module = mcsema::ReadProtoBuf(FLAGS_cfg);
   CHECK(mcsema::LiftCodeIntoModule(cfg_module))
       << "Unable to lift CFG from " << FLAGS_cfg << " into module "
       << FLAGS_output;
