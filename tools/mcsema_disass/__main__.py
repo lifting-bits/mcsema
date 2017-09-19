66531981ecd89a10a5f6e35fec6b4cf3ffab0c9a
Peter Goodman <peter.goodman@gmail.com>
2017-09-19 15:26:22 -0400

    Add avx sub-arch features to the mcsema-disass recognition.

--- tools/mcsema_disass/__main__.py
+++ tools/mcsema_disass/__main__.py
@@ -24,7 +24,9 @@ import textwrap
 
 
 SUPPORTED_OS = ('linux', 'windows',)
-SUPPORTED_ARCH = ('x86', 'amd64', 'aarch64')
+SUPPORTED_ARCH = ('x86', 'x86_avx', 'x86_avx512',
+                  'amd64', 'amd64_avx', 'amd64_avx512',
+                  'aarch64')
 
 def main():
   arg_parser = argparse.ArgumentParser(
