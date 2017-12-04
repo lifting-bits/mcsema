01ab62a9f1bbcb8f77d1a200162cd0dac0e01849
Akshay K <iit.akshay@gmail.com>
2017-12-04 14:10:46 -0500

    update the licence header

--- tests/test_suite_generator/src/linux/ada_trinary/ada_trinary.adb
+++ tests/test_suite_generator/src/linux/ada_trinary/ada_trinary.adb
@@ -1,3 +1,19 @@
+--
+-- Copyright (c) 2017 Trail of Bits, Inc.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--  http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
 with Ada.Text_IO;
 use Ada.Text_IO;
 
@@ -26,7 +42,6 @@ procedure Ada_Trinary is
 
 begin
 	loop
-	    -- Put_Line ("Task 2 " & Integer'Image(Task_Var2) & " Task 1 " & Integer'Image(Task_Var1));
 	    delay Duration(0.1);
     	exit when Task_Var1 = Task_Var2;
  	end loop;
