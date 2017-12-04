cee3bf79b57fd25c898cc7a4308a493ad0caa54a
Akshay K <iit.akshay@gmail.com>
2017-12-04 12:40:04 -0500

    update pthread worker testcase

--- tests/test_suite_generator/src/linux/pthread_worker/main.cpp
+++ tests/test_suite_generator/src/linux/pthread_worker/main.cpp
@@ -10,59 +10,60 @@ class Buffer
   ~Buffer();
 
   void put(bool x) {
-    pthread_mutex_lock(&mtx_);
+    pthread_mutex_lock(&mtx);
 
-    while (is_full_) {
-      pthread_cond_wait(&empty_, &mtx_);
+    while (is_full) {
+      pthread_cond_wait(&empty, &mtx);
     }
 
-    value_ = x;
-    is_full_ = true;
-    pthread_cond_signal(&full_);
-    pthread_mutex_unlock(&mtx_);
+    value = x;
+    is_full = true;
+    pthread_cond_signal(&full);
+    pthread_mutex_unlock(&mtx);
   }
 
   void get(bool &x) {
-    pthread_mutex_lock(&mtx_);
+    pthread_mutex_lock(&mtx);
 
-    while (not is_full_) {
-      pthread_cond_wait(&full_, &mtx_);
+    while (not is_full) {
+      pthread_cond_wait(&full, &mtx);
     }
 
-    x = value_;
-    is_full_ = false;
-    pthread_cond_signal(&empty_);
-    pthread_mutex_unlock(&mtx_);
+    x = value;
+    is_full = false;
+    pthread_cond_signal(&empty);
+    pthread_mutex_unlock(&mtx);
   }
 
  private:
-  bool value_;
-  bool is_full_;
-  pthread_mutex_t mtx_;
-  pthread_cond_t full_;
-  pthread_cond_t empty_;
+  bool value;
+  bool is_full;
+  pthread_mutex_t mtx;
+  pthread_cond_t full;
+  pthread_cond_t empty;
 };
 
 Buffer::Buffer(void) {
-  pthread_mutex_init(&mtx_, NULL);
-  pthread_cond_init(&full_, NULL);
-  pthread_cond_init(&empty_, NULL);
-  value_ = false;
-  is_full_ = false;
+  pthread_mutex_init(&mtx, NULL);
+  pthread_cond_init(&full, NULL);
+  pthread_cond_init(&empty, NULL);
+  value = false;
+  is_full = false;
 }
 
 Buffer::~Buffer() {
-  pthread_mutex_destroy(&mtx_);
-  pthread_cond_destroy(&full_);
-  pthread_cond_destroy(&empty_);
+  pthread_mutex_destroy(&mtx);
+  pthread_cond_destroy(&full);
+  pthread_cond_destroy(&empty);
 }
 
 Buffer buff;
-int iterations;
 
 void *producer(void *arg) {
   int i = 0;
-  for (i = 0; i != iterations - 1; ++i) {
+  unsigned long iter_counter = reinterpret_cast<unsigned long>(arg);
+
+  for (i = 0; i != iter_counter - 1; ++i) {
     buff.put(false);
   }
   buff.put(true);
@@ -83,13 +84,13 @@ void *consumer(void *arg) {
   return arg;
 }
 
-int main(int argc, char * argv[])
-{
-  iterations = atoi(argv[1]);
+int main(int argc, char * argv[]) {
+  (void)argc;
+  int iter_counter = atoi(argv[1]);
 
   pthread_t pro_th;
   pthread_t cons_th;
-  pthread_create(&pro_th, NULL, producer, NULL);
+  pthread_create(&pro_th, NULL, producer, iter_counter);
   pthread_create(&cons_th, NULL, consumer, NULL);
   pthread_join(pro_th, NULL);
   pthread_join(cons_th, NULL);
