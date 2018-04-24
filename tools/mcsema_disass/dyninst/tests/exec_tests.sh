e3690e76579ac72b2c5b223055e8403895cc0be2
Lukáš Korenčik <xkorenc1@fi.muni.cz>
2018-04-24 13:32:55 +0200

    Fix typo in tests

--- tools/mcsema_disass/dyninst/tests/exec_tests.sh
+++ tools/mcsema_disass/dyninst/tests/exec_tests.sh
@@ -3,10 +3,10 @@
 build_path=$1
 
 function run_tests() {
-    ./$1 $build_path/tools/mcsema_disass/dyninst/mcsema-dyninst-disass /home/laky/Programming/Paradise/mc/mcsema/tools/mcsema_disass/defs/linux.txt $build_path/mcsema-lift-4.0 $build_path/mcsema/Arch/X86/Runtime/
+    ./$1 $build_path/tools/mcsema_disass/dyninst/mcsema-dyninst-disass $build_path/../../../tools/mcsema/tools/mcsema_disass/defs/linux.txt $build_path/mcsema-lift-4.0 $build_path/mcsema/Arch/X86/Runtime/
 }
 
 
-./run_tests.py --disass $build_path/tools/mcsema_disass/dyninst/mcsema-dyninst-disass --std_defs /home/laky/Programming/Paradise/mc/mcsema/tools/mcsema_disass/defs/linux.txt --lift $build_path/mcsema-lift-4.0 --lib_dir $build_path/mcsema/Arch/X86/Runtime/
+./run_tests.py --disass $build_path/tools/mcsema_disass/dyninst/mcsema-dyninst-disass --std_defs $build_path/../../../tools/mcsema/tools/mcsema_disass/defs/linux.txt --lift $build_path/mcsema-lift-4.0 --lib_dir $build_path/mcsema/Arch/X86/Runtime/
 
 run_tests "calc_tests.sh"
