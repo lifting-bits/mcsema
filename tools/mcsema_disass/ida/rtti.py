4fbdad5815ce089877f79cf1b532d4f90c6f2796
Akshay K <iit.akshay@gmail.com>
2018-06-21 13:24:41 -0400

    Use generic reference collector

--- tools/mcsema_disass/ida/rtti.py
+++ tools/mcsema_disass/ida/rtti.py
@@ -24,15 +24,33 @@ from util import *
 
 typeinfo_names = [
  "St9type_info@@CXXABI_1.3",
- "__ZTVN10__cxxabiv117__class_type_infoE@@CXXABI_1.3",
- "__ZTVN10__cxxabiv120__si_class_type_infoE@@CXXABI_1.3",
- "__ZTVN10__cxxabiv121__vmi_class_type_infoE@@CXXABI_1.3",
+ "N10__cxxabiv117__class_type_infoE@@CXXABI_1.3",
+ "N10__cxxabiv120__si_class_type_infoE@@CXXABI_1.3",
+ "N10__cxxabiv121__vmi_class_type_infoE@@CXXABI_1.3",
 ]
+
+def vtable_symbol(name):
+  return "__ZTV" + name
+
+def first(val):
+  return idc.FindBinary(0, idc.SEARCH_CASE|idc.SEARCH_DOWN, read_qword(val))
+
+def next(val, ref):
+  return idc.FindBinary(ref+1, idc.SEARCH_CASE|idc.SEARCH_DOWN, read_qword(val))
+
+def find_xrefs(addr):
+  lrefs = list(idautils.DataRefsTo(addr))
+  if len(lrefs) == 0:
+    lrefs = list(idautils.refs(addr, first, next))
+
+  lrefs = [r for r in lrefs if not idc.isCode(idc.GetFlags(r))]
+  return lrefs
 
 def get_typeinfo_refs(name):
   if name is None or name == "":
     return
   
+  name = vtable_symbol(name)
   ea = idc.LocByName(name)
   if is_invalid_ea(ea):
     # try single underscore with the name
@@ -46,8 +64,15 @@ def get_typeinfo_refs(name):
   idx = 0
   while ea != idc.BADADDR:
     DEBUG("Looking for refs to vtable {:x}".format(ea))
-    xrefs = list(idautils.DataRefsTo(ea))
-    ea += get_address_size_in_bytes()*2
+    if idaapi.is_spec_ea(ea):
+      DEBUG("Handling special ea")
+      xrefs = find_xrefs(ea)
+      ea += get_address_size_in_bytes()*2
+      xrefs.extend(find_xrefs(ea))
+    else:
+      ea += get_address_size_in_bytes()*2
+      xrefs = find_xrefs(ea)
+
     for x in xrefs:
       if not is_invalid_ea(x):
         DEBUG("Found {} at {:x}".format(name, x))
