54ac2d724c7b5eb993573389d757c7d55cde0f4f
Lukáš Korenčik <xkorenc1@fi.muni.cz>
2018-07-25 17:34:10 +0200

    Rename files to match the rest of McSema

---
+++ tools/mcsema_disass/dyninst/CFGWriter.h
@@ -0,0 +1,91 @@
+#pragma once
+
+#include "ExternalFunctionManager.h"
+#include "SectionManager.h"
+#include <CFG.pb.h>
+#include <CodeObject.h>
+#include <Expression.h>
+#include <Symtab.h>
+#include <Instruction.h>
+#include <Dereference.h>
+
+#include <unordered_set>
+#include <unordered_map>
+
+
+class CFGWriter {
+public:
+  CFGWriter(mcsema::Module &m, const std::string &moduleName,
+            Dyninst::SymtabAPI::Symtab &symtab,
+            Dyninst::ParseAPI::SymtabCodeSource &symCodeSrc,
+            Dyninst::ParseAPI::CodeObject &codeObj,
+            const ExternalFunctionManager &extFuncMgr);
+
+  void write();
+
+private:
+  /* Don't want to include all functions in binary */ 
+  bool shouldSkipFunction(const std::string &name) const;
+
+  void writeDataVariables(Dyninst::SymtabAPI::Region *region,
+                          mcsema::Segment *segment);
+
+  void writeExternalVariables();
+  void writeGlobalVariables();
+  void writeInternalFunctions();
+  void writeBlock(Dyninst::ParseAPI::Block *block,
+                  Dyninst::ParseAPI::Function *func,
+                  mcsema::Function *cfgInternalFunc);
+  void  writeInstruction(Dyninst::InstructionAPI::Instruction *instruction,
+                        Dyninst::Address addr, mcsema::Block *cfgBlock);
+  void handleCallInstruction(Dyninst::InstructionAPI::Instruction *instruction,
+                             Dyninst::Address addr,
+                             mcsema::Instruction *cfgInstruction);
+  void
+  handleNonCallInstruction(Dyninst::InstructionAPI::Instruction *instruction,
+                           Dyninst::Address addr,
+                           mcsema::Instruction *cfgInstruction);
+  void writeExternalFunctions();
+  void writeInternalData();
+  void writeRelocations( Dyninst::SymtabAPI::Region*, mcsema::Segment* );
+  
+  void immediateNonCall( Dyninst::InstructionAPI::Immediate* imm, Dyninst::Address addr, mcsema::Instruction* cfgInstruction);
+  void dereferenceNonCall( Dyninst::InstructionAPI::Dereference*, Dyninst::Address, mcsema::Instruction*);
+
+
+  std::string getXrefName( Dyninst::Address addr );
+  void xrefsInSegment( Dyninst::SymtabAPI::Region* region, mcsema::Segment* segment );  
+  bool isNoReturn( const std::string& str);
+  void getNoReturns(); 
+
+  void checkDisplacement( Dyninst::InstructionAPI::Instruction*, mcsema::Instruction*);
+  bool isExternal(Dyninst::Address addr) const;
+  const std::string &getExternalName(Dyninst::Address addr) const;
+  
+  /* Tries to work out RegisterAST if there's some reference */
+  bool tryEval(Dyninst::InstructionAPI::Expression *expr,
+               const Dyninst::Address ip, Dyninst::Address &result) const;
+
+
+  /* Dyninst related objects */
+  mcsema::Module &m_module;
+  std::string m_moduleName;
+  Dyninst::SymtabAPI::Symtab &m_symtab;
+  Dyninst::ParseAPI::CodeObject &m_codeObj;
+  Dyninst::ParseAPI::SymtabCodeSource &m_codeSource;
+  
+  /* After -abi-libraries are fully embraced in master branch, this can go out */
+  const ExternalFunctionManager &m_extFuncMgr;
+  SectionManager m_sectionMgr;
+
+  std::set<std::string> m_skipFuncs;
+  
+  std::unordered_map<Dyninst::Offset, std::string> m_funcMap;
+  std::unordered_map< Dyninst::Address, Dyninst::SymtabAPI::Symbol *> m_globalVars;
+  std::unordered_map< Dyninst::Address, Dyninst::SymtabAPI::Symbol *> m_externalVars;
+  std::unordered_map< Dyninst::Address, Dyninst::SymtabAPI::Symbol *> m_segmentVars;
+  
+  
+  std::vector<Dyninst::SymtabAPI::relocationEntry> m_relocations;
+  std::unordered_set<std::string> m_noreturnFunctions; 
+};
---
+++ tools/mcsema_disass/dyninst/ExternalFunc.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <CFG.pb.h>
+#include <cstdint>
+#include <experimental/optional>
+
+class ExternalFunc {
+public:
+  bool operator<(const ExternalFunc &other) const {
+    return m_symbolName < other.m_symbolName;
+  }
+
+  enum class CallingConvention {
+    CallerCleanup = 0,
+    CalleeCleanup = 1,
+    FastCall = 2
+  };
+
+  ExternalFunc() = default;
+  ExternalFunc(const std::string &symbolName, CallingConvention callConv,
+               bool hasReturn, bool noReturn, std::int32_t argCount,
+               bool isWeak,
+               const std::experimental::optional<std::string> &signature = {});
+
+  const std::string &symbolName() const;
+  CallingConvention callingConvention() const;
+  mcsema::ExternalFunction::CallingConvention cfgCallingConvention() const;
+  bool hasReturn() const;
+  bool noReturn() const;
+  std::int32_t argumentCount() const;
+  bool isWeak() const;
+  const std::experimental::optional<std::string> &signature() const;
+
+  void setSymbolName(const std::string &name);
+  void setCallingConvention(CallingConvention cc);
+  void setHasReturn(bool hasReturn);
+  void setNoReturn(bool noReturn);
+  void setArgumentCount(std::int32_t argCount);
+  void setIsWeak(bool isWeak);
+  void setSignature(const std::experimental::optional<std::string> &signature);
+
+private:
+  std::string m_symbolName;
+  CallingConvention m_callConv;
+  bool m_hasReturn;
+  bool m_noReturn;
+  std::int32_t m_argCount;
+  bool m_isWeak;
+  std::experimental::optional<std::string> m_signature;
+};
