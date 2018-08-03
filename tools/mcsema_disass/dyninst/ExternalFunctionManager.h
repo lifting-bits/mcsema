b4370c9a4d293bf593bbb5bbbe89f0316f3225b8
Lukáš Korenčik <xkorenc1@fi.muni.cz>
2018-08-03 17:29:08 +0200

    Big code style sweep

--- tools/mcsema_disass/dyninst/ExternalFunctionManager.h
+++ tools/mcsema_disass/dyninst/ExternalFunctionManager.h
@@ -35,31 +35,31 @@ public:
    */
 
   // Registers a function called "name" with info "func"
-  void addExternalSymbol(const std::string &name, const ExternalFunction &func);
+  void AddExternalSymbol(const std::string &name, const ExternalFunction &func);
 
   // Parses s as if it were a line in a function definitions file
-  void addExternalSymbol(const std::string &s);
+  void AddExternalSymbol(const std::string &s);
 
   // Reads from s as if it were a function definitions file
-  void addExternalSymbols(std::istream &s);
+  void AddExternalSymbols(std::istream &s);
 
   // Un-mark a function as external
-  void removeExternalSymbol(const std::string &name);
+  void RemoveExternalSymbol(const std::string &name);
 
   // Returns true iff the function called "name" is external
-  bool isExternal(const std::string &name) const;
+  bool IsExternal(const std::string &name) const;
 
   // Returns the information stored for the function called "name"
   // and throws an exception if no such function can be found.
-  const ExternalFunction &getExternalFunction(const std::string &name);
+  const ExternalFunction &GetExternalFunction(const std::string &name);
 
   /* The following methods can be used to keep track of the external
    * functions that are actually called somewhere. This can greatly
    * reduce the external_funcs blocks in the CFG file.
    */
-  void clearUsed();
-  void markAsUsed(const std::string &name);
-  std::vector<ExternalFunction> getAllUsed( std::vector<std::string>& uknowns) const;
+  void ClearUsed();
+  void MarkAsUsed(const std::string &name);
+  std::vector<ExternalFunction> GetAllUsed( std::vector<std::string>& uknowns) const;
 
 private:
   std::unordered_map<std::string, ExternalFunction> external_funcs;
