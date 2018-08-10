0dd4a1f4cbaaad602e1c16e8cfeead66acb38da3
Akshay K <iit.akshay@gmail.com>
2018-08-10 15:12:04 -0400

    Split the segement at the runtime external variable boundary

--- tools/mcsema_disass/ida/get_cfg.py
+++ tools/mcsema_disass/ida/get_cfg.py
@@ -1368,7 +1368,11 @@ def identify_program_entrypoints(func_eas):
         if name not in exclude:
           exported_funcs.add(ea)
       else:
-        if name not in exclude and not is_runtime_external_data_reference(ea):
+        # if there is an instance of "Copy of shared data" in the segment, split it at the
+        # symbol boundary. This is to avoid the cross references of these runtime external
+        # variables in the lifted segment variables.
+        # #if name not in exclude: #and not is_runtime_external_data_reference(ea):
+        if name not in exclude:
           exported_vars.add(ea)
 
   DEBUG_POP()
