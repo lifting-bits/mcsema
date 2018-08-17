2487fcd44bef12daabfbf550f1614f3ef736bee1
Lukáš Korenčik <xkorenc1@fi.muni.cz>
2018-08-17 16:26:19 +0200

    No more of .got.plt stubs

--- tools/mcsema_disass/dyninst/SectionManager.h
+++ tools/mcsema_disass/dyninst/SectionManager.h
@@ -2,6 +2,7 @@
 
 #include <Symtab.h>
 #include <set>
+#include <array>
 
 class SectionManager {
 public:
@@ -14,7 +15,25 @@ public:
   std::set<Dyninst::SymtabAPI::Region *> GetAllRegions() { return regions; }
 
   Dyninst::SymtabAPI::Region *getRodata() { return rodata; }
+  Dyninst::SymtabAPI::Region *getText() { return text; }
+
+  Dyninst::SymtabAPI::Region *getRegion(const std::string &name);
+
 private:
+  Dyninst::SymtabAPI::Region *text;
   Dyninst::SymtabAPI::Region *rodata;
   std::set<Dyninst::SymtabAPI::Region *> regions;
+
+  std::array<std::string, 9> to_write_regions = {
+    ".data",
+    ".bss",
+    ".plt",
+    ".init",
+    ".got.plt",
+    ".eh_frame_hdr",
+    ".rodata",
+    ".fini",
+    ".text"
+  };
+  std::set<Dyninst::SymtabAPI::Region *> data_regions;
 };
