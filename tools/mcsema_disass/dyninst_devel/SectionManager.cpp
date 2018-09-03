18550209831f286a3595dc70d0d28c173b97ed74
Lukáš Korenčik <xkorenc1@fi.muni.cz>
2018-09-03 18:43:43 +0200

    SectionManager is now global

--- tools/mcsema_disass/dyninst_devel/SectionManager.cpp
+++ tools/mcsema_disass/dyninst_devel/SectionManager.cpp
@@ -1,38 +1,58 @@
 #include "SectionManager.h"
+#include "Util.h"
+
+#include <CFG.h>
 
 #include <glog/logging.h>
+
 
 using namespace Dyninst;
 using namespace SymtabAPI;
 
+std::unique_ptr<SectionManager> gSection_manager = std::make_unique<SectionManager>();
+
+std::set<Region *> SectionManager::GetAllRegions() {
+  std::set<Region *> result;
+  for (auto &a : regions) {
+    result.insert(a.region);
+  }
+  return result;
+}
+
 Dyninst::SymtabAPI::Region *
 SectionManager::getRegion(const std::string &name) {
-  for (auto r : regions) {
-    if (r->getRegionName() == name) {
-      return r;
+  for (auto &r : regions) {
+    if (r.name == name) {
+      return r.region;
     }
   }
   LOG(FATAL) << "Could not fetch section with name " << name;
 }
 
 void SectionManager::AddRegion(Region *r) {
-  if (regions.find(r) == regions.end()) {
-    LOG(INFO) << "Inserting section " << r->getRegionName();
-    if (r->getRegionName() == ".rodata") {
-      rodata = r;
-    } else if (r->getRegionName() == ".text") {
-      text = r;
-    }
-    regions.insert(r);
-    for (auto &to_write_region : to_write_regions) {
-      if (r->getRegionName() == to_write_region) {
-        data_regions.insert(r);
-      }
+  for (auto &s : regions) {
+    if (s.name == r->getRegionName()) {
+      LOG(INFO) << "Trying to add duplicite section into manager "
+                << r->getRegionName();
+      return;
     }
   }
+  static std::array<std::string, 1> no_write = {
+    ".fini_array",
+  };
+
+  bool should_write = true;
+  for (auto &a : no_write) {
+    if (r->getRegionName() == a) {
+      should_write = false;
+    }
+  }
+  mcsema::Segment *cfg_segment =
+    (should_write) ? gModule.add_segments() : nullptr;
+  regions.push_back({r, r->getRegionName(), cfg_segment, should_write});
 }
 
-bool SectionManager::IsData(Address a) const {
+bool SectionManager::IsData(Address a) {
   const auto &dataRegions{GetDataRegions()};
   const Offset o = static_cast<const Offset>(a);
 
@@ -45,10 +65,10 @@ bool SectionManager::IsData(Address a) const {
   return false;
 }
 
-bool SectionManager::IsCode(Address a) const {
+bool SectionManager::IsCode(Address a) {
   return !IsData(a);
 }
 
-std::set<Region *> SectionManager::GetDataRegions() const {
-  return regions;
+std::set<Region *> SectionManager::GetDataRegions() {
+  return GetAllRegions();
 }
--- tools/mcsema_disass/dyninst_devel/SectionManager.h
+++ tools/mcsema_disass/dyninst_devel/SectionManager.h
@@ -3,37 +3,43 @@
 #include <Symtab.h>
 #include <set>
 #include <array>
+#include <memory>
+
+namespace mcsema {
+  class Segment;
+}
+
+struct Section {
+  Dyninst::SymtabAPI::Region *region;
+  std::string name;
+  mcsema::Segment *cfg_segment = nullptr;
+  bool should_write = true;
+};
 
 class SectionManager {
 public:
   void AddRegion(Dyninst::SymtabAPI::Region *r);
 
-  bool IsData(Dyninst::Address a) const;
-  bool IsCode(Dyninst::Address a) const;
+  bool IsData(Dyninst::Address a);
+  bool IsCode(Dyninst::Address a);
 
-  std::set<Dyninst::SymtabAPI::Region *> GetDataRegions() const;
-  std::set<Dyninst::SymtabAPI::Region *> GetAllRegions() { return regions; }
+  std::set<Dyninst::SymtabAPI::Region *> GetDataRegions();
+  std::set<Dyninst::SymtabAPI::Region *> GetAllRegions();
 
   Dyninst::SymtabAPI::Region *getRodata() { return rodata; }
   Dyninst::SymtabAPI::Region *getText() { return text; }
 
   Dyninst::SymtabAPI::Region *getRegion(const std::string &name);
 
+  static SectionManager &Instance(void) {
+    static SectionManager instance;
+    return instance;
+  }
 private:
   Dyninst::SymtabAPI::Region *text;
   Dyninst::SymtabAPI::Region *rodata;
-  std::set<Dyninst::SymtabAPI::Region *> regions;
 
-  std::array<std::string, 9> to_write_regions = {
-    ".data",
-    ".bss",
-    ".plt",
-    ".init",
-    ".got.plt",
-    ".eh_frame_hdr",
-    ".rodata",
-    ".fini",
-    ".text"
-  };
-  std::set<Dyninst::SymtabAPI::Region *> data_regions;
+  std::vector<Section> regions;
 };
+
+extern std::unique_ptr<SectionManager> gSection_manager;
