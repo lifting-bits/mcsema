8363fef550767fc78ca55b1b4ca39b479c0e1144
Lukáš Korenčik <xkorenc1@fi.muni.cz>
2018-11-01 13:54:38 +0100

    Copyright header

--- tools/mcsema_disass/dyninst/tests/exec_tests.sh
+++ tools/mcsema_disass/dyninst/tests/exec_tests.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Copyright (c) 2018 Trail of Bits, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 build_path=$1
 
 ./run_tests.py --disass $build_path/tools/mcsema_disass/dyninst/mcsema-dyninst-disass --std_defs $build_path/../../../tools/mcsema/tools/mcsema_disass/defs/linux.txt --lift $build_path/mcsema-lift-4.0 --lib_dir $build_path/mcsema/Arch/X86/Runtime/ > /dev/null
--- tools/mcsema_disass/dyninst/tests/pie_s_tests.py
+++ tools/mcsema_disass/dyninst/tests/pie_s_tests.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
 
+# Copyright (c) 2018 Trail of Bits, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Python 2.7 won't work. Implemented with Python 3.5
 
 import unittest
@@ -36,7 +50,7 @@ class LinuxTest (unittest.TestCase):
 
         if ext == ".cpp":
             self.cc = c_cxx
-            std = "-std=c++1z"
+            std = "-std=c++14"
         else:
             std = ""
             self.cc = c_cc
