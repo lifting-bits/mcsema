671e1d7bd18fd9f8acfd89a6817a209aa44faae9
Lukáš Korenčik <xkorenc1@fi.muni.cz>
2019-06-26 20:50:27 +0200

    Batch folder is now created/deleted properly

--- tests/integration_tests/get_cfg.py
+++ tests/integration_tests/get_cfg.py
@@ -15,10 +15,28 @@
 
 import argparse
 import os
-
+import shutil
+import sys
 
 tags_dir="tags"
 
+new = 0
+exclusive = 1
+
+def make_dir(path):
+    print(" > Creating directory: " + path)
+    try:
+        os.makedirs(path)
+    except:
+        print("[Error] could not create directory " + path)
+
+
+def is_batch_dir_sane(batch_name):
+    for filename in os.listdir(batch_name):
+        filename, extension =  os.path.splitext(batch_name)
+        if extension != "cfg" or os.path.isdir(filename):
+            return False
+    return True
 
 def get_binaries_from_flavours(flavors):
     binaries = []
@@ -29,10 +47,30 @@ def get_binaries_from_flavours(flavors):
         with open(os.path.join(tags_dir, filename)) as f:
             for line in f:
                 if line.rstrip("\n") in flavors:
-                    print("> Selecting " + binary_name)
+                    print(" > Selecting " + binary_name)
                     binaries.append(binary_name)
                     break
     return binaries
+
+def create_batch_dir(batch, policy):
+    batch_name = batch + "_cfg"
+    if batch_name not in os.listdir():
+        print(" > Batch name is unique")
+        make_dir(batch_name)
+        return new
+
+    print(" > Batch with same name already exists")
+    print(" > Selected policy is " + policy)
+
+    if policy == "D":
+        print(" > Removing old batch")
+        if is_batch_dir_sane(batch_name):
+            shutil.rmtree(batch_name)
+            make_dir(batch_name)
+        else:
+            print(" > Batch folder is not sane, remove manually")
+            sys.exit(1)
+
 
 def main():
     arg_parser = argparse.ArgumentParser()
@@ -59,10 +97,25 @@ def main():
         default=False,
         required=False)
 
+    arg_parser.add_argument(
+        "--batch",
+        help="Specify batch name",
+        required=True)
+    arg_parser.add_argument(
+        "--batch_policy",
+        choices=["D", "U", "C"],
+        help="How to resolve already existing batch\n D: delete all old cfgs\nU: Update all\nC: create only missing",
+        default="D",
+        required=False)
+
     args, command_args = arg_parser.parse_known_args()
     print( args )
     print( command_args )
 
+    print("Checking batch name")
+    create_batch_dir(args.batch, args.batch_policy)
+
+    print("Select all binaries, specified by flavors")
     binaries = get_binaries_from_flavours(args.flavors)
 
     return 0
