abf3f37893be148df8834ae60be88b95a013f7c0
Lukáš Korenčik <xkorenc1@fi.muni.cz>
2019-06-27 22:50:25 +0200

    run_test cli parsing

---
+++ tests/integration_tests/run_tests.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2018 Trail of Bits, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import os
+import sys
+
+lift = None
+lib_dir = None
+
+batches = []
+shared_lib_dir = None
+libc_dir = None
+
+def check_arguments(args):
+    if not os.path.isfile (args.lift):
+        print("{} passed to --lift is not a valid file".format(args.lift))
+        sys.exit (1)
+    lift = args.lift
+
+    if not os.path.isdir (args.lib_dir):
+        print ("{} passed to --lib_dir is not a valid directory".format(args.lib_dir))
+        sys.exit (1)
+    lib_dir = args.lib_dir
+
+    if not os.path.isdir (args.bin_dir):
+        print("{} passed to --bin_dir is not a valid directory".format(args.bin_dir))
+        sys.exit (1)
+    bin_dir = os.path.abspath(args.bin_dir)
+
+    if not os.path.isdir (args.shared_lib_dir):
+        print("{} passed to --shared_lib_dir is not a valid directory".format(args.shared_lib_dir))
+        sys.exit (1)
+    shared_lib_dir = args.shared_lib_dir
+
+    if not os.path.isdir (args.libc_dir):
+        print ("{} passed to --libc_dir is not a valid directory".format(args.libc_dir))
+        sys.exit (1)
+    libc_dir = args.libc_dir
+
+    # TODO: This whole snippet can be done using argparser and some actions
+    if args.batch is not None:
+        for batch in args.batch:
+            batch_dir = batch + "_cfg"
+            if not os.path.isdir(batch_dir):
+                print("{} passed to --batch is not a valid directory".format(batch))
+                sys.exit(1)
+            batches.append(batch_dir)
+
+    if args.batch_dir is not None:
+        for batch in args.batch_dir:
+            if not os.path.isdir(batch):
+                print("{} passed to --batch_dir is not a valid directory".format(batch))
+                sys.exit(1)
+            batches.append(batch_dir)
+
+    if not batches:
+        print("No batch is selected, exiting")
+        sys.exit(0)
+
+def main():
+    arg_parser = argparse.ArgumentParser (
+        formatter_class = argparse.RawDescriptionHelpFormatter)
+
+    arg_parser.add_argument('--lift',
+                            help = "Path to the mcsema-lift binary",
+                            required = True)
+
+    arg_parser.add_argument('--lib_dir',
+                            help = "Directory that contains libmcsema_rt64.a",
+                            required = True)
+
+    arg_parser.add_argument('--shared_lib_dir',
+                            help = "Directory that contains shared libraries used by original binaries",
+                            default = "shared_libs",
+                            required = False)
+
+
+    arg_parser.add_argument('--bin_dir',
+                            help = "Directory that contains original binaries",
+                            default = "bin",
+                            required = False)
+
+
+    arg_parser.add_argument('--batch',
+                            help = "Batch names",
+                            nargs = '+',
+                            required = False)
+
+    arg_parser.add_argument('--batch_dir',
+                            help = 'Directories with cfgs',
+                            nargs = '+',
+                            required = False)
+
+    arg_parser.add_argument('--libc_dir',
+                            help = "Directory that contains bitcode types of external functions",
+                            required = True)
+
+    arg_parser.add_argument('--dry_run',
+                            help = 'Do not run any tests',
+                            required = False)
+
+
+    args, command_args = arg_parser.parse_known_args ()
+    check_arguments(args)
+
+if __name__ == '__main__':
+   main()
