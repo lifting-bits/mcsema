a0a732a8d0dfd878de482d0ca306b6bfd2fa7306
xkorenc1 <xkorenc1@fi.muni.cz>
2019-11-16 15:57:30 +0100

    Change Scheme to use pointer to db instead of typename.

--- mcsema/CFG/sqlCFG.h
+++ mcsema/CFG/sqlCFG.h
@@ -32,62 +32,61 @@ using Result_ = typename sqlite::Database< db >::QueryResult;
 template< auto db >
 using Database_ = sqlite::Database< db >;
 
-template< const auto &name >
-struct Module {
-  using db = sqlite::Database< name >;
+template< typename Database >
+struct Schema {
   using Query = const char *;
 
-  static void CreateEnums() {
+  static void CreateEnums(Database &db) {
 
     static Query action_enum = R"(create table if not exists exception_frame_actions(
           key integer PRIMARY KEY NOT NULL,
           action text NOT NULL
           ))";
-    db::template query<action_enum>();
+    db.template query<action_enum>();
 
     static Query populate_action_enum =
       R"(insert into exception_frame_actions values(?1, ?2))";
-    db::template query<populate_action_enum>(0, "Cleanup");
-    db::template query<populate_action_enum>(1, "Catch");
+    db.template query<populate_action_enum>(0, "Cleanup");
+    db.template query<populate_action_enum>(1, "Catch");
 
     static Query cc = R"(create table if not exists calling_conventions(
           key integer PRIMARY KEY NOT NULL,
           calling_convention text NOT NULL
           ))";
-    db::template query<cc>();
+    db.template query<cc>();
 
     static Query populate_cc = R"(insert into calling_conventions values(?1, ?2))";
-    db::template query<populate_cc>(0, "CallerCleanup");
-    db::template query<populate_cc>(1, "CalleeCleanup");
-    db::template query<populate_cc>(2, "FastCall");
+    db.template query<populate_cc>(0, "CallerCleanup");
+    db.template query<populate_cc>(1, "CalleeCleanup");
+    db.template query<populate_cc>(2, "FastCall");
 
     static Query operand_types = R"(create table if not exists operand_types(
         key PRIMARY KEY NOT NULL,
         type text
         ))";
-    db::template query<operand_types>();
+    db.template query<operand_types>();
 
     static Query populate_operad_types = R"(insert into operand_types values(?1, ?2))";
-    db::template query<populate_operad_types>(0, "Immediate operand");
-    db::template query<populate_operad_types>(1, "Memory operand");
-    db::template query<populate_operad_types>(2, "MemoryDisplacement operand");
-    db::template query<populate_operad_types>(3, "ControlFlow operand");
-    db::template query<populate_operad_types>(4, "OffsetTable operand");
+    db.template query<populate_operad_types>(0, "Immediate operand");
+    db.template query<populate_operad_types>(1, "Memory operand");
+    db.template query<populate_operad_types>(2, "MemoryDisplacement operand");
+    db.template query<populate_operad_types>(3, "ControlFlow operand");
+    db.template query<populate_operad_types>(4, "OffsetTable operand");
 
 
     static Query locations = R"(create table if not exists locations(
           key integer PRIMARY KEY NOT NULL,
           location text NOT NULL
           ))";
-    db::template query<locations>();
+    db.template query<locations>();
 
     static Query populate_locations = R"(insert into locations values(?1, ?2))";
-    db::template query<populate_locations>(0, "Internal");
-    db::template query<populate_locations>(1, "External");
+    db.template query<populate_locations>(0, "Internal");
+    db.template query<populate_locations>(1, "External");
 
   }
 
-  static void CreateNMTables()
+  static void CreateNMTables(Database &db)
   {
     static Query q_func_2_block =
       R"(create table if not exists func_to_block(
@@ -96,29 +95,29 @@ struct Module {
          FOREIGN KEY(func_ea) REFERENCES functions(ea),
          FOREIGN KEY(bb_ea) REFERENCES blocks(ea)
         ))";
-    db::template query< q_func_2_block >();
+    db.template query< q_func_2_block >();
   }
 
-  static void CreateScheme() {
-    CreateEnums();
+  static void CreateSchema(Database &db) {
+    CreateEnums(db);
 
     static Query c_module_meta =
       R"(create table if not exists module_meta(
          name text,
          arch text,
          os text))";
-    db::template query<c_module_meta>();
+    db.template query<c_module_meta>();
 
     static Query g_vars = R"(create table if not exists global_variables(
           ea integer,
           name text,
           size integer))";
-    db::template query<g_vars>();
+    db.template query<g_vars>();
 
     static Query vars = R"(create table if not exists variables(
           ea integer,
           name text))";
-    db::template query<vars>();
+    db.template query<vars>();
 
     static Query segments = R"(create table if not exists segments(
           ea integer,
@@ -129,7 +128,7 @@ struct Module {
           is_thread_local integer,
           variable_name text
           ))";
-    db::template query<segments>();
+    db.template query<segments>();
 
     static Query stack_vars = R"(create table if not exists stack_variables(
           name text,
@@ -138,7 +137,7 @@ struct Module {
           has_frame integer,
           reg_name text
           ))";
-    db::template query<stack_vars>();
+    db.template query<stack_vars>();
 
     static Query exception_frames = R"(create table if not exists exception_frames(
           func_ea integer,
@@ -147,7 +146,7 @@ struct Module {
           lp_ea integer,
           action NOT NULL REFERENCES exception_frame_actions(key)
           ))";
-    db::template query<exception_frames>();
+    db.template query<exception_frames>();
 
     static Query external_vars = R"(create table if not exists external_variables(
           ea integer,
@@ -156,7 +155,7 @@ struct Module {
           is_weak integer,
           is_thread_local integer
           ))";
-    db::template query<external_vars>();
+    db.template query<external_vars>();
 
     static Query external_functions = R"(create table if not exists external_functions(
           ea integer,
@@ -166,20 +165,20 @@ struct Module {
           is_weak integer,
           signature text
           ))";
-    db::template query<external_functions>();
+    db.template query<external_functions>();
 
     static Query functions = R"(create table if not exists functions(
           ea integer,
           is_entrypoint integer,
           name text
           ))";
-    db::template query<functions>();
+    db.template query<functions>();
 
     static Query blocks = R"(create table if not exists blocks(
           ea integer PRIMARY KEY NOT NULL,
           bytes blob
           ))";
-    db::template query<blocks>();
+    db.template query<blocks>();
 
     static Query code_xrefs = R"(create table if not exists code_references(
           ea integer,
@@ -188,9 +187,9 @@ struct Module {
           mask integer,
           name text
           ))";
-    db::template query<code_xrefs>();
+    db.template query<code_xrefs>();
 
-    CreateNMTables();
+    CreateNMTables(db);
   }
 
 };
@@ -442,11 +441,11 @@ template< const auto &db_name >
 struct Letter_
 {
   sqlite::Database< db_name > _db;
+  using dbT = sqlite::Database< db_name >;
 
-
-  void create_scheme()
+  void CreateSchema()
   {
-    Module<db_name>::CreateScheme();
+    Schema<dbT>::CreateSchema( _db );
   }
 
   auto func()
