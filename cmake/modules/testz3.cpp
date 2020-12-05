57e8625ba80c83c128d62fee51e24e4ec2668159
Eric Kilmer <eric.d.kilmer@gmail.com>
2020-12-05 13:52:29 -0500

    Work with latest remill and anvill

--- cmake/modules/testz3.cpp
+++
@@ -1,8 +0,0 @@
-#include <assert.h>
-#include <z3.h>
-int main() {
-  unsigned int major, minor, build, rev;
-  Z3_get_version(&major, &minor, &build, &rev);
-  printf("%u.%u.%u", major, minor, build);
-  return 0;
-}
